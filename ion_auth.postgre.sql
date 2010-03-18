1f9c9ff4e7cc60b2703a273f998d4ebe1d65d99e
Ben Edmunds <ben.edmunds@gmail.com>
2010-03-17 23:01:33 -0700

    Added default salt and fixed default user data

--- ion_auth.postgre.sql
+++ ion_auth.postgre.sql
@@ -48,5 +48,5 @@ INSERT INTO groups (id, name, description) VALUES
 INSERT INTO meta (id, user_id, first_name, last_name, company, phone) VALUES
     ('1','1','Admin','istrator','ADMIN','0');
     
-INSERT INTO users (id, group_id, ip_address, username, password, email, activation_code, forgotten_password_code, active) VALUES
-    ('1','1','127.0.0.1','administrator','59beecdf7fc966e2f17fd8f65a4a9aeb09d4a3d4','admin@admin.com','',NULL,'1'); 
+INSERT INTO users (id, group_id, ip_address, username, password, salt, email, activation_code, forgotten_password_code, created_on, last_login, active) VALUES
+    ('1','1','127.0.0.1','administrator','59beecdf7fc966e2f17fd8f65a4a9aeb09d4a3d4','9462e8eee0','admin@admin.com','',NULL,'1268889823','1268889823','1'); 
