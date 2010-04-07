3b453f02716626896dabef203ab5b46f7b67640b
Ben Edmunds <ben.edmunds@gmail.com>
2010-04-07 07:31:34 -0700

    Added loading of the ion_auth library into the model constructor to fix the issues with remembered users.  thx Bernd!

--- models/ion_auth_model.php
+++ models/ion_auth_model.php
@@ -64,6 +64,7 @@ class Ion_auth_model extends CI_Model
 	{
 		parent::__construct();
 		$this->load->database();
+                $this->load->library('ion_auth'); 
 		$this->load->config('ion_auth', TRUE);
 		$this->load->helper('cookie');
 		$this->load->helper('date');
