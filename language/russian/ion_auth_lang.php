10e2ff385eb2ded6605d97a1460af694f4e0c348
Ben Edmunds <ben.edmunds@gmail.com>
2010-04-28 23:19:34 -0400

    Added russian language file - via for@petrosyan.rv.ua

---
+++ language/russian/ion_auth_lang.php
@@ -0,0 +1,51 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+* Name:  Ion Auth Lang - Russian (UTF-8)
+*
+* Author: Ben Edmunds
+* 		  ben.edmunds@gmail.com
+*         @benedmunds
+* Translation:  Petrosyan R.
+*             for@petrosyan.rv.ua
+*
+* Location: http://github.com/benedmunds/ion_auth/
+*
+* Created:  03.26.2010
+*
+* Description:  Russian language file for Ion Auth messages and errors
+*
+*/
+
+// Account Creation
+$lang['account_creation_successful'] 	  	 = 'Учетная запись успешно создана';
+$lang['account_creation_unsuccessful'] 	 	 = 'Невозможно создать учетную запись';
+$lang['account_creation_duplicate_email'] 	 = 'Электронная почта используется или некорректна';
+$lang['account_creation_duplicate_username'] 	 = 'Имя пользователя существует или некорректно';
+
+// Password
+$lang['password_change_successful'] 	 	 = 'Пароль успешно изменен';
+$lang['password_change_unsuccessful'] 	  	 = 'Пароль невозможно изменить';
+$lang['forgot_password_successful'] 	 	 = 'Пароль сброшен. На электронную почту отправлено сообщение';
+$lang['forgot_password_unsuccessful'] 	 	 = 'Невозможен сброс пароля';
+
+// Activation
+$lang['activate_successful'] 		  	 = 'Учетная запись активирована';
+$lang['activate_unsuccessful'] 		 	 = 'Не удалось активировать учетную запись';
+$lang['deactivate_successful'] 		  	 = 'Учетная запись деактивирована';
+$lang['deactivate_unsuccessful'] 	  	 = 'Невозможно деактивировать учетную запись';
+$lang['activation_email_successful'] 	  	 = 'Сообщение об активации отправлено';
+$lang['activation_email_unsuccessful']   	 = 'Сообщение об активации невозможно отправить';
+
+// Login / Logout
+$lang['login_successful'] 		  	 = 'Авторизация прошла успешно';
+$lang['login_unsuccessful'] 		  	 = 'Логин не верен';
+$lang['logout_successful'] 		 	 = 'Выход успешный';
+
+// Account Changes
+$lang['update_successful'] 		 	 = 'Учетная запись успешно обновлена';
+$lang['update_unsuccessful'] 		 	 = 'Невозможно обновить учетную запись';
+$lang['delete_successful'] 		 	 = 'Учетная запись удалена';
+$lang['delete_unsuccessful'] 		 	 = 'Невозможно удалить учетную запись';
+
+
+?>
