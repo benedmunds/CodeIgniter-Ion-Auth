7c704a3d0892850a6336360e7c70f133650c785b
Ben Edmunds <ben.edmunds@gmail.com>
2010-05-01 17:24:02 -0400

    Added Turkish language file (via Acipayamli Ozi)

---
+++ language/turkish/ion_auth_lang.php
@@ -0,0 +1,50 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+* Name:  Ion Auth Lang - Turkish (UTF-8)
+*
+* Author: Ben Edmunds
+* 	  ben.edmunds@gmail.com
+*         @benedmunds
+* Translation:  Acipayamli Ozi
+*             
+* Location: http://github.com/benedmunds/ion_auth/
+*
+* Created:  05.01.2010
+*
+* Description:  Turkish language file for Ion Auth messages and errors
+*/
+
+// Account Creation
+$lang['account_creation_successful'] 	  	 = 'Üyelik Kaydınız Başarıyla Tamamlandı';
+$lang['account_creation_unsuccessful'] 	 	 = 'Üyelik Kaydınız Yapılamadı';
+$lang['account_creation_duplicate_email'] 	 = 'E-mail Adresi Geçersiz ya da Daha Önceden Alınmış';
+$lang['account_creation_duplicate_username']	 = 'Kullanıcı Adı Geçersiz ya da Daha Önceden Alınmış';
+
+
+// Password
+$lang['password_change_successful'] 	 	 = 'Şifreniz Değiştirildi';
+$lang['password_change_unsuccessful'] 	  	 = 'Şifreniz Değiştirelemedi';
+$lang['forgot_password_successful'] 	 	 = 'Yen Şifreniz E-mail Adresinize Yollandı';
+$lang['forgot_password_unsuccessful'] 	 	 = 'Şifreniz Değiştirilemedi';
+
+// Activation
+$lang['activate_successful'] 		  	 = 'Hesap Kullanıma Açıldı';
+$lang['activate_unsuccessful'] 		 	 = 'Hesap Kullanıma Açılamadı';
+$lang['deactivate_successful'] 		  	 = 'Hesap Kullanıma Kapatıldı';
+$lang['deactivate_unsuccessful'] 	  	 = 'Hesap Kullanıma Kapatılamadı';
+$lang['activation_email_successful'] 		 = 'Aktivasyon Maili Yollandı';
+$lang['activation_email_unsuccessful']  	 = 'Aktivasyon Maili Yollanamadı';
+
+// Login / Logout
+$lang['login_successful'] 		  	 = 'Giriş Başarılı';
+$lang['login_unsuccessful'] 		 	 = 'Giriş Başarısız';
+$lang['logout_successful'] 		 	 = 'Çıkış Başarılı';
+  
+// Account Changes
+$lang['update_successful'] 		 	 = 'Üyelik Bilgileri Güncellendi';
+$lang['update_unsuccessful'] 		 	 = 'Üyelik Bilgileri Güncellenemedi';
+$lang['delete_successful'] 		 	 = 'Kullanıcı Silindi';
+$lang['delete_unsuccessful'] 			 = 'Kullanıcı Silinemedi';
+
+
+?>
