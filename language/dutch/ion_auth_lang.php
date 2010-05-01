09a3811a05b02c9f13fce78aca7f42976cf5a93e
Jeroen van der Gulik <jeroen@isset.nl>
2010-05-02 06:37:09 +0800

    Added Dutch language file

---
+++ language/dutch/ion_auth_lang.php
@@ -0,0 +1,49 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+* Name:  Ion Auth Lang - Dutch
+* 
+* Author: Jeroen van der Gulik
+*         jeroen@isset.nl
+* 
+* Location: http://github.com/benedmunds/CodeIgniter-Ion-Auth
+*          
+* Created:  05.01.2010 
+* 
+* Description:  Dutch language file for Ion Auth messages and errors
+* 
+*/
+
+// Account Creation
+$lang['account_creation_successful'] 	  	 = 'Account is aangemaakt';
+$lang['account_creation_unsuccessful'] 	 	 = 'Account aanmaken is mislukt';
+$lang['account_creation_duplicate_email'] 	 = 'Email is al in gebruik of ongeldig';
+$lang['account_creation_duplicate_username'] 	 = 'Username is al in gebruik of ongeldig';
+
+
+// Password
+$lang['password_change_successful'] 	 	 = 'Password is verandert';
+$lang['password_change_unsuccessful'] 	  	 = 'Password veranderen is mislukt';
+$lang['forgot_password_successful'] 	 	 = 'Email om het Password te Resetten is verzonden';
+$lang['forgot_password_unsuccessful'] 	 	 = 'Password Resetten is mislukt';
+
+// Activation
+$lang['activate_successful'] 		  	 = 'Account is geactiveerd';
+$lang['activate_unsuccessful'] 		 	 = 'Account activeren is mislukt';
+$lang['deactivate_successful'] 		  	 = 'Account is gedeactiveerd';
+$lang['deactivate_unsuccessful'] 	  	 = 'Accound deactiveren is mislukt';
+$lang['activation_email_successful'] 	  	 = 'Activatie Email is verzonden';
+$lang['activation_email_unsuccessful']   	 = 'Activatie Email verzenden is mislukt';
+
+// Login / Logout
+$lang['login_successful'] 		  	 = 'U bent ingelogged';
+$lang['login_unsuccessful'] 		  	 = 'Login is incorrect';
+$lang['logout_successful'] 		 	 = 'U bent uitgelogged';
+  
+// Account Changes
+$lang['update_successful'] 		 	 = 'Account informatie is bijgewerkt';
+$lang['update_unsuccessful'] 		 	 = 'Account informatie bijwerken is mislukt';
+$lang['delete_successful'] 		 	 = 'User is verwijdert';
+$lang['delete_unsuccessful'] 		 	 = 'User verwijderen is mislukt';
+
+/* End of file ion_auth_lang.php */
+/* Location: ./system/application/language/dutch/ion_auth_lang.php */
