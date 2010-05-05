ee0e6ae2b39fbc495926dc0f67134284b840cc7e
Ben Edmunds <ben.edmunds@gmail.com>
2010-05-05 00:51:06 -0400

    Added spanish language file (via @wilfridogarcia)

---
+++ language/spanish/ion_auth_lang.php
@@ -0,0 +1,50 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+* Name:  Ion Auth Lang - Spanish
+* 
+* Author: Wilfrido García Espinosa
+* 		  contacto@wilfridogarcia.com
+*         @wilfridogarcia
+* 
+* Location: http://github.com/benedmunds/ion_auth/
+*          
+* Created:  05.04.2010 
+* 
+* Description:  Spanish language file for Ion Auth messages and errors
+* 
+*/
+
+// Account Creation
+$lang['account_creation_successful'] 	  	 = 'Cuenta Exitósamente Creada';
+$lang['account_creation_unsuccessful'] 	 	 = 'No se Puede Crear la Cuenta';
+$lang['account_creation_duplicate_email'] 	 = 'Email en Uso o Inválido';
+$lang['account_creation_duplicate_username'] 	 = 'Nombre de Usuario en Uso o Inválido';
+
+
+// Password
+$lang['password_change_successful'] 	 	 = 'Password Exitósamente Cambiado';
+$lang['password_change_unsuccessful'] 	  	 = 'No se Puede Cambiar el Password';
+$lang['forgot_password_successful'] 	 	 = 'Nuevo Password Enviado por Email';
+$lang['forgot_password_unsuccessful'] 	 	 = 'No se Puede Crear Nuevo Password';
+
+// Activation
+$lang['activate_successful'] 		  	 = 'Cuenta Activada';
+$lang['activate_unsuccessful'] 		 	 = 'No se Puede Activar Cuenta';
+$lang['deactivate_successful'] 		  	 = 'Cuenta Desactivada';
+$lang['deactivate_unsuccessful'] 	  	 = 'No se Puede Desactivar la Cuenta';
+$lang['activation_email_successful'] 	  	 = 'Email de Activación Enviado';
+$lang['activation_email_unsuccessful']   	 = 'No se Puede Enviar Email de Activación';
+
+// Login / Logout
+$lang['login_successful'] 		  	 = 'Sesión Iniciada Exitósamente';
+$lang['login_unsuccessful'] 		  	 = 'No se Puede Iniciar Sesión';
+$lang['logout_successful'] 		 	 = 'Sesión Terminada Exitósamente';
+  
+// Account Changes
+$lang['update_successful'] 		 	 = 'Información de Cuenta Exitósamente Actualizada';
+$lang['update_unsuccessful'] 		 	 = 'No se Puede Actualizar la Información de Cuenta';
+$lang['delete_successful'] 		 	 = 'Usuario Borrado';
+$lang['delete_unsuccessful'] 		 	 = 'No se Puede Borrar Usuario';
+
+
+?>
