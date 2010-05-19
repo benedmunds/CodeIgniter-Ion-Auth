c5df3300f3193307c958c3b8c61d99088de12b5e
Ben Edmunds <ben.edmunds@gmail.com>
2010-05-18 21:51:24 -0400

    Added Portuguese language file (via André B. Simões)

---
+++ language/portuguese/ion_auth_lang.php
@@ -0,0 +1,49 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+* Name:  Ion Auth Lang - Portuguese
+* 
+* Author: André Brás Simões
+* 		  andrebrassimoes@gmail.com
+*         
+* 
+* Location: http://github.com/benedmunds/ion_auth/
+*          
+* Created:  17.05.2010 
+* 
+* Description:  Portuguese language file for Ion Auth messages and errors
+* 
+*/
+
+// Account Creation
+$lang['account_creation_successful'] 	  	 = 'Conta Criada com Sucesso';
+$lang['account_creation_unsuccessful'] 	 	 = 'Não é possível criar a Conta';
+$lang['account_creation_duplicate_email'] 	 = 'Email em Uso ou Inválido';
+$lang['account_creation_duplicate_username'] 	 = 'Nome de Usuário em Uso ou Inválido';
+
+
+// Password
+$lang['password_change_successful'] 	 	 = 'Password Alterada com Sucesso';
+$lang['password_change_unsuccessful'] 	  	 = 'Não é Possível Alterar a Password';
+$lang['forgot_password_successful'] 	 	 = 'Nova Password Enviada por Email';
+$lang['forgot_password_unsuccessful'] 	 	 = 'Não é Possível Criar uma Nova Password';
+
+// Activation
+$lang['activate_successful'] 		  	 = 'Conta Activada';
+$lang['activate_unsuccessful'] 		 	 = 'Não é Possível Activar a Conta';
+$lang['deactivate_successful'] 		  	 = 'Conta Desactivada';
+$lang['deactivate_unsuccessful'] 	  	 = 'Não é Possível Desactivar a Conta';
+$lang['activation_email_successful'] 	  	 = 'Email de Activação Enviado';
+$lang['activation_email_unsuccessful']   	 = 'Não é Possível Enviar o Email de Activação';
+
+// Login / Logout
+$lang['login_successful'] 		  	 = 'Sessão Iniciada com Sucesso';
+$lang['login_unsuccessful'] 		  	 = 'Não é Possível Iniciar Sessão';
+$lang['logout_successful'] 		 	 = 'Sessão Terminada com Sucesso';
+  
+// Account Changes
+$lang['update_successful'] 		 	 = 'Informação da Conta Actualizada com Sucesso';
+$lang['update_unsuccessful'] 		 	 = 'Não é Possível Actualizar a Informação da Conta';
+$lang['delete_successful'] 		 	 = 'Utilizador Removido';
+$lang['delete_unsuccessful'] 		 	 = 'Não é Possível Remover o Utilizador';
+
+?>
