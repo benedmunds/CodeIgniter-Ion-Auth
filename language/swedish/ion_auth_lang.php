608e5b05baea2905ad1fcca759ff34b5a0655af2
Ben Edmunds <ben.edmunds@gmail.com>
2011-01-09 02:13:11 -0500

    Added swedish language (via David V. Wallin)

---
+++ language/swedish/ion_auth_lang.php
@@ -0,0 +1,47 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+* Name:  Ion Auth Lang - English
+* 
+* Author: Ben Edmunds
+* 		  ben.edmunds@gmail.com
+*         @benedmunds
+* 
+* Location: http://github.com/benedmunds/ion_auth/
+*
+* Created:  03.14.2010 
+* 
+* Description:  English language file for Ion Auth messages and errors
+*
+*/
+
+// Account Creation
+$lang['account_creation_successful'] 	  	 = 'Kontot har nu Skapats';
+$lang['account_creation_unsuccessful'] 	 	 = 'Det gick inte att skapa kontot';
+$lang['account_creation_duplicate_email'] 	 = 'Epostadressen är fel eller används redan';
+$lang['account_creation_duplicate_username'] 	 = 'Användarnamnet är fel eller används redan';
+
+
+// Password
+$lang['password_change_successful'] 	 	 = 'Lösenordet har nu ändrats';
+$lang['password_change_unsuccessful'] 	  	 = 'Det gick inte att ändra lösenordet';
+$lang['forgot_password_successful'] 	 	 = 'Mail för återställning av lösenord har nu skickats';
+$lang['forgot_password_unsuccessful'] 	 	 = 'Det gick inte att återställa lösenordet';
+
+// Activation
+$lang['activate_successful'] 		  	 = 'Kontot Aktiverades';
+$lang['activate_unsuccessful'] 		 	 = 'Det gick inte att Aktivera kontot';
+$lang['deactivate_successful'] 		  	 = 'Kontot Inaktiverades';
+$lang['deactivate_unsuccessful'] 	  	 = 'Det gick inte att Inaktivera kontot';
+$lang['activation_email_successful'] 	  	 = 'Aktiveringsmail har skickats';
+$lang['activation_email_unsuccessful']   	 = 'Det gick inte att skicka Aktiveringsmailet';
+
+// Login / Logout
+$lang['login_successful'] 		  	 = 'Du är nu Inloggad';
+$lang['login_unsuccessful'] 		  	 = 'Fel Inlogg';
+$lang['logout_successful'] 		 	 = 'Du är nu Utloggad';
+  
+// Account Changes
+$lang['update_successful'] 		 	 = 'Kontouppgifterna Uppdaterades korrekt';
+$lang['update_unsuccessful'] 		 	 = 'Det gick inte att uppdatera Kontouppgifterna';
+$lang['delete_successful'] 		 	 = 'Användaren är borttagen';
+$lang['delete_unsuccessful'] 		 	 = 'Det gick inte att ta bort användaren';
