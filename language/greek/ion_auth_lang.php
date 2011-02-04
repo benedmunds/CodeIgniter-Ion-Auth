8618722ce92075db17c740e717ceafd627aea403
Ben Edmunds <ben.edmunds@gmail.com>
2011-02-04 16:39:20 -0600

    Added Greek language (via osci)

---
+++ language/greek/ion_auth_lang.php
@@ -0,0 +1,46 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+* Name:  Ion Auth Lang - Greek
+* 
+* Author: Vagelis Papaloukas
+* 		  vagelispapalou@yahoo.gr
+* 
+* Location: http://github.com/benedmunds/ion_auth/
+*          
+* Created:  02.04.2011
+* 
+* Description:  Greek language file for Ion Auth messages and errors
+* 
+*/
+
+// Account Creation
+$lang['account_creation_successful'] 	  	 = 'Ο Λογαριασμός Δημιουργήθηκε Επιτυχώς';
+$lang['account_creation_unsuccessful'] 	 	 = 'Αποτυχία Δημιουργίας Λογαριασμού';
+$lang['account_creation_duplicate_email'] 	 = 'Το Email χρησιμποιείται ήδη ή είναι λάθος';
+$lang['account_creation_duplicate_username'] 	 = 'Ο Χρήστης υπάρχει ήδη ή είναι λάθος';
+
+
+// Password
+$lang['password_change_successful'] 	 	 = 'Επιτυχής Αλλαγή Κωδικού';
+$lang['password_change_unsuccessful'] 	  	 = 'Αδυναμία Αλλαγής Κωδικού';
+$lang['forgot_password_successful'] 	 	 = 'Εστάλη Email Κωδικού Επαναφοράς';
+$lang['forgot_password_unsuccessful'] 	 	 = 'Αδυναμία Επαναφοράς Κωδικού';
+
+// Activation
+$lang['activate_successful'] 		  	 = 'Ο Λογαριασμός Ενεργοποιήθηκε';
+$lang['activate_unsuccessful'] 		 	 = 'Αδυναμία Ενεργοποίησης Λογαριασμού';
+$lang['deactivate_successful'] 		  	 = 'Ο Λογαριασμός Απενεργοποιήθηκε';
+$lang['deactivate_unsuccessful'] 	  	 = 'Αδυναμία Απενεργοποίησης Λογαριασμού';
+$lang['activation_email_successful'] 	  	 = 'Εστάλη Email Ενεργοποίησης Λογαριασμού';
+$lang['activation_email_unsuccessful']   	 = 'Αδυναμία Αποστολής Email Ενεργοποίησης';
+
+// Login / Logout
+$lang['login_successful'] 		  	 = 'Συνδεθήκατε Επιτυχώς';
+$lang['login_unsuccessful'] 		  	 = 'Λάθος Στοιχεία';
+$lang['logout_successful'] 		 	 = 'Αποσυνδεθήκατε Επιτυχώς';
+  
+// Account Changes
+$lang['update_successful'] 		 	 = 'Οι Πληροφορίες του Λογαριασμού Ενημερώθηκαν Επιτυχώς';
+$lang['update_unsuccessful'] 		 	 = 'Αδυναμία Ενημέρωσης Πληροφοριών Λογαριασμού';
+$lang['delete_successful'] 		 	 = 'Ο Χρήστης Διαγράφηκε';
+$lang['delete_unsuccessful'] 		 	 = 'Αδυναμία Διαγραφής Χρήστη';
