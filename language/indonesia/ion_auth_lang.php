338420d15cd3682abab7fe2d4ee00cc7dc2a8f74
Ben Edmunds <ben.edmunds@gmail.com>
2011-05-14 21:43:00 -0500

    Added Indonesian lang (via Deden Fathurahman)

---
+++ language/indonesia/ion_auth_lang.php
@@ -0,0 +1,47 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+* Name:  Ion Auth Lang - Indonesian
+* 
+* Author: Deden Fathurahman
+* 		  dedenf@gmail.com
+*         @dedenf
+* 
+* Location: http://github.com/benedmunds/ion_auth/
+*          
+* Created:  03.14.2010 
+* 
+* Description:  Indonesian Language file for Ion Auth Message and error
+* 
+*/
+
+// Account Creation
+$lang['account_creation_successful'] 	  	 = 'Akun Telah Sukses Dibuat';
+$lang['account_creation_unsuccessful'] 	 	 = 'Tidak Dapat Membuat Akun';
+$lang['account_creation_duplicate_email'] 	 = 'Email Ada Yang Telah Menggunakan Atau Salah Email';
+$lang['account_creation_duplicate_username'] 	 = 'Username Telah Ada Yang Menggunakan Atau Salah';
+
+
+// Password
+$lang['password_change_successful'] 	 	 = 'Password Telah Diubah';
+$lang['password_change_unsuccessful'] 	  	 = 'Tidak Dapat Mengubah Password';
+$lang['forgot_password_successful'] 	 	 = 'Email Untuk Reset Password Telah Dikirim';
+$lang['forgot_password_unsuccessful'] 	 	 = 'Tidak Dapat Mereset Password';
+
+// Activation
+$lang['activate_successful'] 		  	 = 'Akun Telah Diaktivasi';
+$lang['activate_unsuccessful'] 		 	 = 'Tidak Dapat Mengaktivasi Akun';
+$lang['deactivate_successful'] 		  	 = 'Akun Telah Dinonaktifkan';
+$lang['deactivate_unsuccessful'] 	  	 = 'Tidak Dapat Menonaktifkan Akun';
+$lang['activation_email_successful'] 	  	 = 'Email Aktivasi Telah Dikirim';
+$lang['activation_email_unsuccessful']   	 = 'Tidak Dapat Mengirim Email Aktivasi';
+
+// Login / Logout
+$lang['login_successful'] 		  	 = 'Login Sukses';
+$lang['login_unsuccessful'] 		  	 = 'Login Gagal';
+$lang['logout_successful'] 		 	 = 'Sukses Keluar';
+  
+// Account Changes
+$lang['update_successful'] 		 	 = 'Informasi Akun Telah Sukses Diperbaharui';
+$lang['update_unsuccessful'] 		 	 = 'Tidak Dapat Memperbaharui Informasi Akun';
+$lang['delete_successful'] 		 	 = 'User Telah Dihapus';
+$lang['delete_unsuccessful'] 		 	 = 'Gagal Menghapus User';
