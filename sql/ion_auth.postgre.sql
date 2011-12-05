6171ca474caf7cce23f45b4200c35bb74536fdfe
Mark Davidson <mark@4each.co.uk>
2011-12-05 12:26:29 +0000

    Fixed initial inserts for users and users_groups
     - By manually setting the id of the user in the INSERT query it
       results in the sequence not being incremented correctly. So have
       removed it from being manually set, since a new sequence will be
       created automatically starting at 1 the administrator will end up
       with an id of 1 anyway.
     - When inserting the users_groups the same senario applies where
       settings ids manually don't increment the sequence so the id being
       set manually for those inserts has also  been removed.

--- sql/ion_auth.postgre.sql
+++ sql/ion_auth.postgre.sql
@@ -45,9 +45,9 @@ INSERT INTO groups (id, name, description) VALUES
     (1,'admin','Administrator'),
     (2,'members','General User');
     
-INSERT INTO users (id, ip_address, username, password, salt, email, activation_code, forgotten_password_code, created_on, last_login, active, first_name, last_name, company, phone) VALUES
-    ('1','127.0.0.1','administrator','59beecdf7fc966e2f17fd8f65a4a9aeb09d4a3d4','9462e8eee0','admin@admin.com','',NULL,'1268889823','1268889823','1','Admin','istrator','ADMIN','0'); 
+INSERT INTO users (ip_address, username, password, salt, email, activation_code, forgotten_password_code, created_on, last_login, active, first_name, last_name, company, phone) VALUES
+    ('127.0.0.1','administrator','59beecdf7fc966e2f17fd8f65a4a9aeb09d4a3d4','9462e8eee0','admin@admin.com','',NULL,'1268889823','1268889823','1','Admin','istrator','ADMIN','0');
 
-INSERT INTO users_groups (id, user_id, group_id) VALUES
-    (1,1,1),
-    (2,1,2);
+INSERT INTO users_groups (user_id, group_id) VALUES
+    (1,1),
+    (1,2);
