7572bbfd05d9895a7d3a8c9bb828b16553ca57cb
Ben Edmunds <ben.edmunds@gmail.com>
2012-04-27 11:29:13 -0400

    Fixed security issue in clear_forgotten_password for pullrequest/issue #200

--- models/ion_auth_model.php
+++ models/ion_auth_model.php
@@ -442,7 +442,6 @@ class Ion_auth_model extends CI_Model
 			$data = array(
 				'password'                => $this->hash_password($password, $salt),
 				'forgotten_password_code' => NULL,
-				'active'                  => 1,
 				'forgotten_password_time' => NULL
 			 );
 
