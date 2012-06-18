a310417775bceb3aeda57d3ba8dd61ae35da4d42
daparky <daparky21@googlemail.com>
2012-06-19 01:52:53 +0200

    If you have an update user form with password field and leave it blank it will set the password field to '0'

--- models/ion_auth_mongodb_model.php
+++ models/ion_auth_mongodb_model.php
@@ -1512,7 +1512,15 @@ class Ion_auth_mongodb_model extends CI_Model {
 		// Hash new password
 		if (array_key_exists('password', $data))
 		{
-			$data['password'] = $this->hash_password($data['password'], $user->salt);
+			if( ! empty($data['password']))
+			{
+				$data['password'] = $this->hash_password($data['password'], $user->salt);
+			}
+			else
+			{
+				// unset password so it doesn't effect database entry if password field empty
+				unset($data['password']);
+			}
 		}
 
 		// TODO: DO WE NEED TO CHECK EMAIL AND USERNAME VALUES REGARDLESS
