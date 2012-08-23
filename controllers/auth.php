f32cfdcfe06160571085291c31dec31a0f98c5f6
MAMProgr <mamprogr@gmail.com>
2012-08-23 04:01:17 +0300

    Merge IonAuth login with HybridIgniter,  login_provider();

--- controllers/auth.php
+++ controllers/auth.php
@@ -99,6 +99,139 @@ class Auth extends CI_Controller {
 			$this->load->view('auth/login', $this->data);
 		}
 	}
+	
+	//log the user in by provider
+	function login_provider($provider = '')
+	{
+		if(empty($provider)) redirect();
+		try
+		{
+			$this->load->library('HybridAuthLib');
+
+			if ($this->hybridauthlib->serviceEnabled($provider))
+			{
+				$service = $this->hybridauthlib->authenticate($provider);
+				
+				if ($service->isUserConnected())
+				{
+					$user_profile = $service->getUserProfile();
+					/************************************************************************************/
+					$provider_uid = $user_profile->identifier;
+					if($this->ion_auth->login_by_provider($provider,$provider_uid))
+					{
+						$data['user_profile'] = $this->ion_auth->user_by_provider($provider,$provider_uid);
+						$this->load->view('auth/user_profile',$data);
+					}
+					else
+					{
+						$username = $user_profile->firstName.' '.$user_profile->lastName;
+						$password = rand(8, 15);
+						$email = $user_profile->email;
+						
+						$additional_data['profileURL']	= $user_profile->profileURL;
+						$additional_data['webSiteURL']	= $user_profile->webSiteURL;
+						$additional_data['photoURL']	= $user_profile->photoURL;
+						$additional_data['displayName']	= $user_profile->displayName;
+						$additional_data['description']	= $user_profile->description;
+						$additional_data['firstName']	= $user_profile->firstName;
+						$additional_data['lastName']	= $user_profile->lastName;
+						$additional_data['gender']		= $user_profile->gender;
+						$additional_data['language']	= $user_profile->language;
+						$additional_data['age']			= $user_profile->age;
+						$additional_data['birthDay']	= $user_profile->birthDay;
+						$additional_data['birthMonth']	= $user_profile->birthMonth;
+						$additional_data['birthYear']	= $user_profile->birthYear;
+						$additional_data['email']		= $user_profile->email;
+						$additional_data['emailVerified']	= $user_profile->emailVerified;
+						$additional_data['phone']		= $user_profile->phone;
+						$additional_data['address']		= $user_profile->address;
+						$additional_data['country']		= $user_profile->country;
+						$additional_data['region']		= $user_profile->region;
+						$additional_data['city']		= $user_profile->city;
+						$additional_data['zip']			= $user_profile->zip;
+						
+						if($email != null && $this->ion_auth->register_by_provider($provider, $provider_uid, $username, $password, $email,  $additional_data))
+						{
+							if($this->ion_auth->login_by_provider($provider,$provider_uid))
+							{
+								$data['user_profile'] = $this->ion_auth->user_by_provider($provider,$provider_uid);
+								$this->load->view('hauth/done',$data);
+							}
+							else
+							{
+								//if the login was un-successful
+								//redirect them back to the login page
+								$this->session->set_flashdata('message', $this->ion_auth->errors());
+								redirect('auth/login', 'refresh'); //use redirects instead of loading views for compatibility with MY_Controller libraries
+							}
+						}
+						else
+						{
+							//if the register was un-successful
+							//redirect them back to the login page
+							$this->session->set_flashdata('message', $this->ion_auth->errors());
+							redirect('auth/login', 'refresh'); //use redirects instead of loading views for compatibility with MY_Controller libraries
+						}
+					}
+					/************************************************************************************/
+				}
+				else // Cannot authenticate user
+				{
+					$this->data['message'] = $this->session->set_flashdata('message', 'Cannot authenticate user');
+					
+					$this->load->view('auth/login', $this->data);
+				}
+			}
+			else // This service is not enabled.
+			{
+				show_404($_SERVER['REQUEST_URI']);
+			}
+		}
+		catch(Exception $e)
+		{
+			$error = 'Unexpected error';
+			switch($e->getCode())
+			{
+				case 0 : $error = 'Unspecified error.'; break;
+				case 1 : $error = 'Hybriauth configuration error.'; break;
+				case 2 : $error = 'Provider not properly configured.'; break;
+				case 3 : $error = 'Unknown or disabled provider.'; break;
+				case 4 : $error = 'Missing provider application credentials.'; break;
+				case 5 : log_message('debug', 'controllers.HAuth.login: Authentification failed. The user has canceled the authentication or the provider refused the connection.');
+				         //redirect();
+				         if (isset($service))
+				         {
+				         	log_message('debug', 'controllers.HAuth.login: logging out from service.');
+				         	$service->logout();
+				         }
+				         show_error('User has cancelled the authentication or the provider refused the connection.');
+				         break;
+				case 6 : $error = 'User profile request failed. Most likely the user is not connected to the provider and he should to authenticate again.';
+				         break;
+				case 7 : $error = 'User not connected to the provider.';
+				         break;
+			}
+
+			if (isset($service))
+			{
+				$service->logout();
+			}
+			
+			$this->data['message'] = $this->session->set_flashdata('message', $error);
+			
+			$this->load->view('auth/login', $this->data);
+		}
+	}
+
+	// important for HybridIgniter library..
+	public function provider_endpoint()
+	{
+		if ($_SERVER['REQUEST_METHOD'] === 'GET')
+		{
+			$_GET = $_REQUEST;
+		}
+		require_once APPPATH.'/third_party/hybridauth/index.php';
+	}
 
 	//log the user out
 	function logout()
