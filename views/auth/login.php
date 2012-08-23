8cef4070e3c9eb9beada79c0c2d6415b7c58009d
MAMProgr <mamprogr@gmail.com>
2012-08-23 03:56:49 +0300

    Add login with links..

--- views/auth/login.php
+++ views/auth/login.php
@@ -2,7 +2,7 @@
 <p>Please login with your email/username and password below.</p>
 	
 <div id="infoMessage"><?php echo $message;?></div>
-
+<?php if(isset($identity) && isset($password)): ?>
 <?php echo form_open("auth/login");?>
   	
   <p>
@@ -24,5 +24,16 @@
   <p><?php echo form_submit('submit', 'Login');?></p>
     
 <?php echo form_close();?>
+<?php endif; ?>
 
 <p><a href="forgot_password">Forgot your password?</a></p>
+<?php
+	$this->load->helper('url');
+	echo anchor('auth/login_provider/Google','Login With Google.').'<br />';
+	
+	echo anchor('auth/login_provider/Twitter','Login With Twitter.').'<br />';
+	
+	echo anchor('auth/login_provider/Facebook','Login With Facebook.').'<br />';
+	
+	echo anchor('auth/login_provider/LinkedIn','Login With LinkedIn.').'<br />';
+?>
