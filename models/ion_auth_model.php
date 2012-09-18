2cbc4dde01b6a35ac666c7a87b18ab7660ebe509
Daniel <daniel.rener@oyova.com>
2012-09-18 00:22:43 -0400

    Fixed some issues with SQL server

--- models/ion_auth_model.php
+++ models/ion_auth_model.php
@@ -1774,7 +1774,7 @@ class Ion_auth_model extends CI_Model
 	}
 	
 	protected function _prepare_ip($ip_address) {
-		if ($this->db->platform() === 'postgre')
+		if ($this->db->platform() === 'postgre' || $this->db->platform() === 'sqlsrv' || $this->db->platform() === 'mssql')
 		{
 			return $ip_address;
 		}
