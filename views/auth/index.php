a27bba6a058f670a00c747c56385a7b3bc1e3ce3
Raul Baldner Junior <raul_baldner_junior@hotmail.com>
2012-10-02 09:42:33 -0300

    Created controller user with functions extracted from controller auth. Various small changes on the proccess, including correct handling of error messages, etc.. Views changed accordingly.

--- views/auth/index.php
+++ views/auth/index.php
@@ -1,10 +1,11 @@
-<h1>Users</h1>
+<h1><?php echo $title;?></h1>
 <p>Below is a list of the users.</p>
 
 <div id="infoMessage"><?php echo $message;?></div>
 
 <table cellpadding=0 cellspacing=10>
 	<tr>
+		<th>Actions</th>
 		<th>First Name</th>
 		<th>Last Name</th>
 		<th>Email</th>
@@ -13,6 +14,7 @@
 	</tr>
 	<?php foreach ($users as $user):?>
 		<tr>
+			<td><?php echo anchor("user/read/$user->id", 'View'), ' ', anchor("user/update/$user->id", 'Update'), ' ', anchor("user/delete/$user->id", 'Delete'); ?>
 			<td><?php echo $user->first_name;?></td>
 			<td><?php echo $user->last_name;?></td>
 			<td><?php echo $user->email;?></td>
@@ -21,9 +23,9 @@
 					<?php echo $group->name;?><br />
                 <?php endforeach?>
 			</td>
-			<td><?php echo ($user->active) ? anchor("auth/deactivate/".$user->id, 'Active') : anchor("auth/activate/". $user->id, 'Inactive');?></td>
+			<td><?php echo ($user->active) ? anchor("user/deactivate/".$user->id, 'Active') : anchor("user/activate/". $user->id, 'Inactive');?></td>
 		</tr>
 	<?php endforeach;?>
 </table>
 
-<p><a href="<?php echo site_url('auth/create_user');?>">Create a new user</a></p>
+<p><a href="<?php echo site_url('user/create');?>">Create a new user</a></p>
