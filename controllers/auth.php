414ab45a34844d3062a0d678db1ac3ffda4538c5
Raul Baldner Junior <raul_baldner_junior@hotmail.com>
2012-10-02 10:04:40 -0300

    Controller auth don't list users
    Removed users listing from the index of auth controller.
    When accessing the index, the flash message (if there is one) is kept
    and the page is redirected to user controller

--- controllers/auth.php
+++ controllers/auth.php
@@ -22,32 +22,8 @@ class Auth extends CI_Controller {
 	//redirect if needed, otherwise display the user list
 	function index()
 	{
-
-		if (!$this->ion_auth->logged_in())
-		{
-			//redirect them to the login page
-			redirect('auth/login', 'refresh');
-		}
-		elseif (!$this->ion_auth->is_admin())
-		{
-			//redirect them to the home page because they must be an administrator to view this
-			redirect('/', 'refresh');
-		}
-		else
-		{
-			//set the flash data error message if there is one
-			$this->data['message'] = (validation_errors()) ? validation_errors() : $this->session->flashdata('message');
-
-			//list the users
-			$this->data['users'] = $this->ion_auth->users()->result();
-			foreach ($this->data['users'] as $k => $user)
-			{
-				$this->data['users'][$k]->groups = $this->ion_auth->get_users_groups($user->id)->result();
-			}
-
-
-			$this->load->view('auth/index', $this->data);
-		}
+		$this->session->keep_flashdata('message');
+		redirect('user', 'refresh');
 	}
 
 	//log the user in
