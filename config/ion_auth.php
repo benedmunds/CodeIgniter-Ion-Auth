ada6e1d0337e3c8e826bed2d043c36880b6681ec
Matthew Urtnowski <murtnowski@gmail.com>
2012-10-08 11:43:45 -0700

    Implemented Temporary Lockout
    Added feature to lock out an identity for a number of seconds if they
    exceeded the maximum login attempts.  It has it's own custom warning in
    the english language file, but it needs to be added to the other
    languages as well

--- config/ion_auth.php
+++ config/ion_auth.php
@@ -95,6 +95,7 @@ $config['user_expire']          = 86500; 				// How long to remember the user (s
 $config['user_extend_on_login'] = FALSE; 				// Extend the users cookies everytime they auto-login
 $config['track_login_attempts'] = FALSE;				// Track the number of failed login attempts for each user or ip.
 $config['maximum_login_attempts']     = 3; 				// The maximum number of failed login attempts.
+$config['lockout_time'] = 600;				   			// The number of seconds to lockout an account due to exceeded attempts
 $config['forgot_password_expiration'] = 0; 				// The number of seconds after which a forgot password request will expire. If set to 0, forgot password requests will not expire.
 
 
