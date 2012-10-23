c608795458943914ca184fc92487f02469f31d05
Ben Edmunds <ben.edmunds@gmail.com>
2012-10-23 13:45:22 -0500

    Updated spanish translation (via ibarrajo).  Closes PR #336

--- language/spanish/ion_auth_lang.php
+++ language/spanish/ion_auth_lang.php
@@ -1,52 +1,52 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /**
 * Name:  Ion Auth Lang - Spanish
-*
-* Author: Julio Napurí
-* 		  julionc@gmail.com
-*         @julionc
-*
+* 
+* Author: Wilfrido Garc�a Espinosa
+* 		  contacto@wilfridogarcia.com
+*         @wilfridogarcia
+* 
 * Location: http://github.com/benedmunds/ion_auth/
-*
-* Created:  03.14.2010
-*
+*          
+* Created:  05.04.2010 
+* 
 * Description:  Spanish language file for Ion Auth messages and errors
-*
+* 
 */
 
 // Account Creation
-$lang['account_creation_successful'] 	  	 = 'La cuenta ha sido creada';
-$lang['account_creation_unsuccessful'] 	 	 = 'No se puede crear la cuenta';
+$lang['account_creation_successful'] 	  	 = 'Cuenta creada con éxito';
+$lang['account_creation_unsuccessful'] 	 	 = 'No se ha podido crear la cuenta';
 $lang['account_creation_duplicate_email'] 	 = 'Email en uso o inválido';
 $lang['account_creation_duplicate_username'] = 'Nombre de usuario en uso o inválido';
 
+
 // Password
-$lang['password_change_successful'] 	 	 = 'La contraseña ha sido cambiada correctamente';
-$lang['password_change_unsuccessful'] 	  	 = 'No se puede cambiar la constraseña';
-$lang['forgot_password_successful'] 	 	 = 'Enviado correo electrónico para restablecer contraseña';
-$lang['forgot_password_unsuccessful'] 	 	 = 'No se puede restablecer la contraseña';
+$lang['password_change_successful'] 	 	 = 'Contraseña renovada éxitosamente';
+$lang['password_change_unsuccessful'] 	  	 = 'No se ha podido cambiar la contraseña';
+$lang['forgot_password_successful'] 	 	 = 'Nueva contraseña enviada por email';
+$lang['forgot_password_unsuccessful'] 	 	 = 'No se ha podido crear una nueva contraseña';
 
 // Activation
 $lang['activate_successful'] 		  	     = 'Cuenta activada';
-$lang['activate_unsuccessful'] 		 	     = 'No se puede activar la cuenta';
+$lang['activate_unsuccessful'] 		 	     = 'No se ha podido activar la cuenta';
 $lang['deactivate_successful'] 		  	     = 'Cuenta desactivada';
-$lang['deactivate_unsuccessful'] 	  	     = 'No se puede desactivar la cuenta';
-$lang['activation_email_successful'] 	  	 = 'Recibirás un correo electrónico de activación de la cuenta';
-$lang['activation_email_unsuccessful']   	 = 'No se puede enviar el correo electrónico de activación';
+$lang['deactivate_unsuccessful'] 	  	     = 'No se ha podido desactivar la cuenta';
+$lang['activation_email_successful'] 	  	 = 'Email de activación enviado';
+$lang['activation_email_unsuccessful']   	 = 'No se ha podido enviar el email de activación';
 
 // Login / Logout
-$lang['login_successful'] 		  	         = 'Sesión iniciada';
-$lang['login_unsuccessful'] 		  	     = 'Inicio de sesión incorrecto';
-$lang['login_unsuccessful_not_active'] 		 = 'La cuenta está inactiva';
-$lang['logout_successful'] 		 	         = 'Se ha cerrado la sesión';
+$lang['login_successful'] 		      	     = 'Sesión iniciada con éxito';
+$lang['login_unsuccessful'] 		  	     = 'No se ha podido iniciar sesión';
+$lang['logout_successful'] 		 	         = 'Sesión finalizada con éxito';
 
 // Account Changes
-$lang['update_successful'] 		 	         = 'La información de la cuenta se ha actualizado correctamente';
-$lang['update_unsuccessful'] 		 	     = 'No se puede actualizar la información de la cuenta';
-$lang['delete_successful'] 		 	         = 'El usuario ha sido borrado';
-$lang['delete_unsuccessful'] 		 	     = 'No se puede borrar el usuario';
+$lang['update_successful'] 		 	         = 'Información de la cuenta actualizada con éxito';
+$lang['update_unsuccessful'] 		 	     = 'No se ha podido actualizar la información de la cuenta';
+$lang['delete_successful'] 		 	         = 'Usuario eliminado';
+$lang['delete_unsuccessful'] 		 	     = 'No se ha podido Eliminar el usuario';
 
 // Email Subjects
-$lang['email_forgotten_password_subject']    = 'Verificación de la contraseña';
-$lang['email_new_password_subject']          = 'Nueva contraseña';
+$lang['email_forgotten_password_subject']    = 'Verificación de contraseña olvidada';
+$lang['email_new_password_subject']          = 'Nueva Contraseña';
 $lang['email_activation_subject']            = 'Activación de la cuenta';
