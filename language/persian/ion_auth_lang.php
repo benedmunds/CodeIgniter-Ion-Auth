b563f9ad8d7d28a7f9ddbe4601a78b78a7ac9197
Ghasem Shahabi <ghasem.shahabi@gmail.com>
2012-11-20 18:36:47 +0330

    Some improvements in Persian translation
    There was some errors in previous translation

--- language/persian/ion_auth_lang.php
+++ language/persian/ion_auth_lang.php
@@ -3,58 +3,63 @@
 * Name:  Ion Auth Lang - Persian
 *
 * Author: Rob Davarnia
-* 		  robdvr@gmail.com
+*         robdvr@gmail.com
 *         @robdvr
+*         
+* Modification: Ghasem Shahabi
+* 		  ghasem.shahabi@gmail.com
+* 		  @GhasemShahabi
 *
 * Location: http://github.com/benedmunds/ion_auth/
 *
 * Created:  10.24.2012
+* Modified: 11.20.2012
 *
 * Description:  Persian language file for Ion Auth messages and errors
 *
 */
 
 // Account Creation
-$lang['account_creation_successful'] 	  	 = 'کاربر با موفقیت ساخته شد';
-$lang['account_creation_unsuccessful'] 	 	 = 'کاربر با موفقیت ساخته نشد';
-$lang['account_creation_duplicate_email'] 	 = 'ایمیل در حال استفاده است';
-$lang['account_creation_duplicate_username'] = 'نام کاربری در حال استفاده است';
+$lang['account_creation_successful'] 	  	 = 'حساب کاربري با موفقیت ايجاد شد';
+$lang['account_creation_unsuccessful'] 	 	 = 'ايجاد حساب کاربري با شكست مواجه شد';
+$lang['account_creation_duplicate_email'] 	 = 'ایمیل قبلا استفاده شده است';
+$lang['account_creation_duplicate_username'] = 'نام کاربری قبلا استفاده شده است';
 
 // Password
 $lang['password_change_successful'] 	 	 = 'رمز عبور عوض شد';
-$lang['password_change_unsuccessful'] 	  	 = 'رمز عبور عوض نشد';
-$lang['forgot_password_successful'] 	 	 = 'ایمیل برای رمز جدید فرستاده شد';
-$lang['forgot_password_unsuccessful'] 	 	 = 'ایمیل برای رمز جدید فرستاده نشد';
+$lang['password_change_unsuccessful'] 	  	 = 'تعويض رمز عبور انجام نشد';
+$lang['forgot_password_successful'] 	 	 = 'ایمیل تعويض رمز عبور ارسال شد';
+$lang['forgot_password_unsuccessful'] 	 	 = 'امكان تعويض رمز عبور وجود ندارد';
 
 // Activation
-$lang['activate_successful'] 		  	     = 'کاربر فعال شد';
-$lang['activate_unsuccessful'] 		 	     = 'کاربر فعال نشد';
-$lang['deactivate_successful'] 		  	     = 'کاربر غیر فعال شد';
-$lang['deactivate_unsuccessful'] 	  	     = 'کاربر غیر فعال نشد';
+$lang['activate_successful'] 		  	     = 'حساب کاربري فعال شد';
+$lang['activate_unsuccessful'] 		 	     = 'امكان فعال سازي حساب کاربري وجود ندارد';
+$lang['deactivate_successful'] 		  	     = 'حساب کاربري غيرفعال شد';
+$lang['deactivate_unsuccessful'] 	  	     = 'امكان غيرفعال كردن حساب کاربري وجود ندارد';
 $lang['activation_email_successful'] 	  	 = 'ایمیل فعال سازی فرستاده شد';
-$lang['activation_email_unsuccessful']   	 = 'ایمیل فعال سازی فرستاده نشد';
+$lang['activation_email_unsuccessful']   	 = 'امكان ارسال ایمیل فعال سازی وجود ندارد';
 
 // Login / Logout
-$lang['login_successful'] 		  	         = 'ورود با موفقیت';
+$lang['login_successful'] 		  	         = 'ورود موفقیت آميز';
 $lang['login_unsuccessful'] 		  	     = 'ورود نا موفق';
-$lang['login_unsuccessful_not_active'] 		 = 'کاربر غیر فعال است';
-$lang['login_timeout']                       = 'کاربر قفل است';
-$lang['logout_successful'] 		 	         = 'خروج با موفقیت';
+$lang['login_unsuccessful_not_active'] 		 = 'حساب کاربري غیر فعال است';
+$lang['login_timeout']                       = 'حساب کاربري موقتا قفل شده است، لطفا بعدا دوباره تلاش نماييد.';
+$lang['logout_successful'] 		 	         = 'خروج موفقیت آميز';
 
 // Account Changes
-$lang['update_successful'] 		 	         = 'اطلاعات کاربر به روز شد';
-$lang['update_unsuccessful'] 		 	     = 'اطلاعات کاربر به روز نشد';
+$lang['update_successful'] 		 	         = 'اطلاعات حساب کاربري به روز شد';
+$lang['update_unsuccessful'] 		 	     = 'اطلاعات حساب کاربري به روز نشد';
 $lang['delete_successful']              	 = 'کاربر حذف شد';
-$lang['delete_unsuccessful']          		 = 'کاربر حذف نشد';
+$lang['delete_unsuccessful']          		 = 'امكان حذف کاربر وجود ندارد';
 
 // Groups
-$lang['group_creation_successful']  = 'گروه حذف شد';
-$lang['group_already_exists']       = 'گروه در حال استفاده است';
-$lang['group_update_successful']    = 'گروه حذف شد';
-$lang['group_delete_successful']    = 'گروه پاک شد';
-$lang['group_delete_unsuccessful'] 	= 'گروه پاک نشد';
+$lang['group_creation_successful']  = 'گروه با موفقيت ايجاد شد';
+$lang['group_already_exists']       = 'اين نام گروه قبلا استفاده شده است';
+$lang['group_update_successful']    = 'جزئيات گروه با موفقيت بروز رساني شد';
+$lang['group_delete_successful']    = 'گروه حذف شد';
+$lang['group_delete_unsuccessful'] 	= 'امكان حذف گروه وجود ندارد';
 
 // Email Subjects
 $lang['email_forgotten_password_subject']    = 'تایید رمز عبور جدید';
-$lang['email_new_password_subject']          = 'رمز جدید';
-$lang['email_activation_subject']            = 'فعال سازی کاربر';
+$lang['email_new_password_subject']          = 'رمز عبور جدید';
+$lang['email_activation_subject']            = 'فعال سازی حساب کاربري';
