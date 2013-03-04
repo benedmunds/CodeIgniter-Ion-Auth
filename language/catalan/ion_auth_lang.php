0629c89a99da2ab5599b09aae59891f37a911194
Ben Edmunds <ben.edmunds@gmail.com>
2013-03-04 15:51:18 -0600

    Updated catalan language file (via Oriol Navascuez

--- language/catalan/ion_auth_lang.php
+++ language/catalan/ion_auth_lang.php
@@ -24,10 +24,10 @@ $lang['account_creation_duplicate_username']	= 'Nom d&#39;usuari en ús o invàl
 
 
 // Password
-$lang['password_change_successful']				= 'Password canviat amb èxit';
-$lang['password_change_unsuccessful']			= 'No ha estat possible canviar el password';
-$lang['forgot_password_successful']				= 'Nou password enviat per email';
-$lang['forgot_password_unsuccessful']			= 'No ha estat possible crear un nou password';
+$lang['password_change_successful']				= 'Contrasenya canviada amb èxit';
+$lang['password_change_unsuccessful']			= 'No ha estat possible canviar la contrasenya';
+$lang['forgot_password_successful']				= 'Nova contrasenya enviada per email';
+$lang['forgot_password_unsuccessful']			= 'No ha estat possible crear una nova contrasenya';
 
 // Activation
 $lang['activate_successful']					= 'Compte activat';
@@ -49,9 +49,6 @@ $lang['delete_successful']						= 'Usuari eliminat';
 $lang['delete_unsuccessful']					= 'No s&#39;ha pogut Eliminar l&#39;usuari';
 
 // Email Subjects - TODO Please Translate
-$lang['email_forgotten_password_subject']    = 'Forgotten Password Verification';
-$lang['email_new_password_subject']          = 'New Password';
-$lang['email_activation_subject']            = 'Account Activation';
-
-
-?>
+$lang['email_forgotten_password_subject']    = 'Verificació de contrasenya oblidada';
+$lang['email_new_password_subject']          = 'Nova contrasenya';
+$lang['email_activation_subject']            = 'Activació del compte';
