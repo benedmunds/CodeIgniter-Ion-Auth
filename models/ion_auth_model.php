2ae7f4dd9d1364c6b44067c3044a6caa3536fa1d
52aa456eef8b60ad6754b31fbdcc77bb <52aa456eef8b60ad6754b31fbdcc77bb@hushmail.com>
2013-03-22 10:37:46 -0300

    Update ion_auth_model.php

--- models/ion_auth_model.php
+++ models/ion_auth_model.php
@@ -11,7 +11,12 @@
 * Location: http://github.com/benedmunds/CodeIgniter-Ion-Auth
 *
 * Created:  10.01.2009
+* 
+* Last Change: 3.22.13
 *
+* Changelog:
+* * 3-22-13 - Additional entropy added - 52aa456eef8b60ad6754b31fbdcc77bb
+* 
 * Description:  Modified auth system based on redux_auth with extensive customization.  This is basically what Redux Auth 2 should be.
 * Original Author name has been kept but that does not mean that the method has not been modified.
 *
