5293cb01b874b5d37c2bda2ea336eadf3789f922
Nathan Pitman <nathan@ninefour.co.uk>
2013-04-03 22:23:01 +0100

    Corrected typo in comments, yes I'm anal like that.

--- config/ion_auth.php
+++ config/ion_auth.php
@@ -166,8 +166,8 @@ $config['store_salt']  = FALSE;
  */
 $config['message_start_delimiter'] = '<p>'; 	// Message start delimiter
 $config['message_end_delimiter']   = '</p>'; 	// Message end delimiter
-$config['error_start_delimiter']   = '<p>';		// Error mesage start delimiter
-$config['error_end_delimiter']     = '</p>';	// Error mesage end delimiter
+$config['error_start_delimiter']   = '<p>';		// Error message start delimiter
+$config['error_end_delimiter']     = '</p>';	// Error message end delimiter
 
 /* End of file ion_auth.php */
 /* Location: ./application/config/ion_auth.php */
