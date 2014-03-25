814f19837627a9fd9257cfd4866dac9e49f25938
Ben Edmunds <ben.edmunds@gmail.com>
2014-03-24 18:08:03 -0700

    Fixed issue with default group creation on register
    Fixes issue #563

--- models/ion_auth_model.php
+++ models/ion_auth_model.php
@@ -861,7 +861,7 @@ class Ion_auth_model extends CI_Model
 
 		//add to default group if not already set
 		$default_group = $this->where('name', $this->config->item('default_group', 'ion_auth'))->group()->row();
-		if ((isset($default_group->id) && !isset($groups)) || (!empty($groups) && !in_array($default_group->id, $groups)))
+		if ((isset($default_group->id) && empty($groups)) || (!empty($groups) && !in_array($default_group->id, $groups)))
 		{
 			$this->add_to_group($default_group->id, $id);
 		}
