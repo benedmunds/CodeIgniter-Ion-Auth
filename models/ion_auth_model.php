9d922d75b5debbbd6750f1071193b500cf85e7f9
Nathan Pitman <nathan@ninefour.co.uk>
2014-05-16 13:17:22 +0100

    Removed escape_str from log-in query

--- models/ion_auth_model.php
+++ models/ion_auth_model.php
@@ -950,7 +950,7 @@ class Ion_auth_model extends CI_Model
 		$this->trigger_events('extra_where');
 
 		$query = $this->db->select($this->identity_column . ', username, email, id, password, active, last_login')
-		                  ->where($this->identity_column, $this->db->escape_str($identity))
+		                  ->where($this->identity_column, $identity)
 		                  ->limit(1)
 		                  ->get($this->tables['users']);
 
