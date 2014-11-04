ad607a8d9e9f179eefa2d812dda245500938ec7f
oldhobbyist <oldhobbyist@users.noreply.github.com>
2014-11-04 10:42:46 -0600

    Update auth.php
    Remove duplicate line.

--- controllers/auth.php
+++ controllers/auth.php
@@ -10,8 +10,6 @@ class Auth extends CI_Controller {
 		$this->load->library('form_validation');
 		$this->load->helper('url');
 
-		$this->load->database();
-
 		$this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
 
 		$this->lang->load('auth');
