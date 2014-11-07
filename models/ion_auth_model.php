e457e8f65f635b73e9c6c8c628e64a28342348cc
solmelkonian <solmelkonian@gmail.com>
2014-11-07 13:21:51 -0800

    Update ion_auth_model.php

--- models/ion_auth_model.php
+++ models/ion_auth_model.php
@@ -706,6 +706,7 @@ class Ion_auth_model extends CI_Model
 		$this->trigger_events('extra_where');
 
 		return $this->db->where('username', $username)
+				->order_by("id", "ASC")
 				->limit(1)
 		                ->count_all_results($this->tables['users']) > 0;
 	}
@@ -728,6 +729,7 @@ class Ion_auth_model extends CI_Model
 		$this->trigger_events('extra_where');
 
 		return $this->db->where('email', $email)
+				->order_by("id", "ASC")
 				->limit(1)
 		                ->count_all_results($this->tables['users']) > 0;
 	}
