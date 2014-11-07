fc56630515061a66ef84a22a908044b0ef5e8ec3
solmelkonian <solmelkonian@gmail.com>
2014-11-07 01:23:29 -0800

    Update ion_auth_model.php
    Add a LIMIT 1 to the email_check() and username_check() functions because we only need to find one row

--- models/ion_auth_model.php
+++ models/ion_auth_model.php
@@ -706,6 +706,7 @@ class Ion_auth_model extends CI_Model
 		$this->trigger_events('extra_where');
 
 		return $this->db->where('username', $username)
+				->limit(1)
 		                ->count_all_results($this->tables['users']) > 0;
 	}
 
@@ -727,6 +728,7 @@ class Ion_auth_model extends CI_Model
 		$this->trigger_events('extra_where');
 
 		return $this->db->where('email', $email)
+				->limit(1)
 		                ->count_all_results($this->tables['users']) > 0;
 	}
 
