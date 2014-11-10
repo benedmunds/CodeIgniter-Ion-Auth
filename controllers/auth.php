7ccbe60876fcfcb53828fe7abc25ef50d14c95af
Ben Edmunds <ben.edmunds@gmail.com>
2014-11-10 13:43:41 -0800

    Update auth.php
    Fixed missing parens from recent merge

--- controllers/auth.php
+++ controllers/auth.php
@@ -6,8 +6,8 @@ class Auth extends CI_Controller {
 	{
 		parent::__construct();
 		$this->load->database();
-		$this->load->library(array('ion_auth','form_validation');
-		$this->load->helper(array('url','language');
+		$this->load->library(array('ion_auth','form_validation'));
+		$this->load->helper(array('url','language'));
 
 		$this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
 
