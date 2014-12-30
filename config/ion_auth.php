1d125e8726ee0f454bfbac4d5ef04fce8d7682e1
Tim Joosten <Topairy@gmail.com>
2014-12-30 08:07:05 +0100

    Add variable explanation

--- config/ion_auth.php
+++ config/ion_auth.php
@@ -80,24 +80,46 @@ $config['salt_prefix']    = '$2y$';
  | used by $this->ion_auth->is_max_login_attempts_exceeded().
  | The controller should check this function and act
  | appropriately. If this variable set to 0, there is no maximum.
+ |
+ | -------------------------------------------------------------------------
+ | Explanation of the variables
+ | -------------------------------------------------------------------------
+ | $config['site_title'] = Site title, example.com
+ | $config['admin_email'] = Admin Email, admin@example.com
+ | $config['default_group'] = Default group, use name
+ | $config['admin_group'] = Default administrators group, use name
+ | $config['identity'] = A database column which is used to login with
+ | $config['min_password_length'] = Minimun required length of password
+ | $config['max_password_length'] = Maximum allowed length of password
+ | $config['email_activation'] = Email activation for registration
+ | $config['manual_activation'] = Manual activation for registration
+ | $config['remember_users'] = Allow users to be remembered and enable auto-login
+ | $config['user_expire'] = How long to remember the user (seconds). Set to zero for no expiration
+ | $config['user_extend_on_login'] = Extend the users cookies every time they auto-login
+ | $config['track_login_attempts'] = Track the number of failed login attempts for each user or IP.
+ | $config['track_login_ip_address'] = Track login attempts by IP address, if FALSE will track based on identity. (Default: TRUE)
+ | $config['maximum_login_attempts'] = the maximum number of failed login attempts.
+ | $config['lockout_time'] = The number of seconds to lockout an account due to exceeded attempts
+ | $config['forget_password_expiration'] = The number of milliseconds after which a forgot password request will expire. If set to 0, forgot password requests will not expire.
+ | -------------------------------------------------------------------------
  */
-$config['site_title']                 = "Example.com";       // Site Title, example.com
-$config['admin_email']                = "admin@example.com"; // Admin Email, admin@example.com
-$config['default_group']              = 'members';           // Default group, use name
-$config['admin_group']                = 'admin';             // Default administrators group, use name
-$config['identity']                   = 'email';             // A database column which is used to login with
-$config['min_password_length']        = 8;                   // Minimum Required Length of Password
-$config['max_password_length']        = 20;                  // Maximum Allowed Length of Password
-$config['email_activation']           = FALSE;               // Email Activation for registration
-$config['manual_activation']          = FALSE;               // Manual Activation for registration
-$config['remember_users']             = TRUE;                // Allow users to be remembered and enable auto-login
-$config['user_expire']                = 86500;               // How long to remember the user (seconds). Set to zero for no expiration
-$config['user_extend_on_login']       = FALSE;               // Extend the users cookies every time they auto-login
-$config['track_login_attempts']       = FALSE;               // Track the number of failed login attempts for each user or ip.
-$config['track_login_ip_address']     = TRUE;                // Track login attempts by IP Address, if FALSE will track based on identity. (Default: TRUE)
-$config['maximum_login_attempts']     = 3;                   // The maximum number of failed login attempts.
-$config['lockout_time']               = 600;                 // The number of seconds to lockout an account due to exceeded attempts
-$config['forgot_password_expiration'] = 0;                   // The number of milliseconds after which a forgot password request will expire. If set to 0, forgot password requests will not expire.
+$config['site_title']                 = "Example.com";
+$config['admin_email']                = "admin@example.com";
+$config['default_group']              = 'members';
+$config['admin_group']                = 'admin';
+$config['identity']                   = 'email';
+$config['min_password_length']        = 8;
+$config['max_password_length']        = 20;
+$config['email_activation']           = FALSE;
+$config['manual_activation']          = FALSE;
+$config['remember_users']             = TRUE;
+$config['user_expire']                = 86500;
+$config['user_extend_on_login']       = FALSE;
+$config['track_login_attempts']       = FALSE;
+$config['track_login_ip_address']     = TRUE;
+$config['maximum_login_attempts']     = 3;
+$config['lockout_time']               = 600;
+$config['forgot_password_expiration'] = 0;
 
 /*
  | -------------------------------------------------------------------------
@@ -173,12 +195,23 @@ $config['store_salt']  = FALSE;
  | -------------------------------------------------------------------------
  | Message Delimiters.
  | -------------------------------------------------------------------------
+ | Message delimeters
+ |
+ | -------------------------------------------------------------------------
+ | Explanation of the variables
+ | -------------------------------------------------------------------------
+ | $config['delimiters_source'] = "config" = use the settings defined here, "form_validation" = use the settings defined in CI's form validation library
+ | $config['message_start_delimiter'] = Message start delimiter
+ | $config['message_end_delimiter'] = Message end delimiter
+ | $config['error_start_delimiter'] = Error message start delimiter
+ | $config['error_end_delimiter'] =  Error message end delimiter
+ | -------------------------------------------------------------------------
  */
-$config['delimiters_source']       = 'config'; 	// "config" = use the settings defined here, "form_validation" = use the settings defined in CI's form validation library
-$config['message_start_delimiter'] = '<p>'; 	// Message start delimiter
-$config['message_end_delimiter']   = '</p>'; 	// Message end delimiter
-$config['error_start_delimiter']   = '<p>';		// Error message start delimiter
-$config['error_end_delimiter']     = '</p>';	// Error message end delimiter
+$config['delimiters_source']       = 'config';
+$config['message_start_delimiter'] = '<p>';
+$config['message_end_delimiter']   = '</p>';
+$config['error_start_delimiter']   = '<p>';
+$config['error_end_delimiter']     = '</p>';
 
 /* End of file ion_auth.php */
 /* Location: ./application/config/ion_auth.php */
