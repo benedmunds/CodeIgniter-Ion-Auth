d7a01ed34c4c87032013752d646a891c98af7869
Tim Joosten <Topairy@gmail.com>
2014-12-30 08:24:20 +0100

    fixes bug that i made

--- views/auth/index.php
+++ views/auth/index.php
@@ -20,7 +20,7 @@
 			<td>
 				<?php foreach ($user->groups as $group):?>
 					<?php echo anchor("auth/edit_group/".$group->id, htmlspecialchars($group->name,ENT_QUOTES,'UTF-8')); ?><br />
-        <?php endforeach(); ?>
+        <?php endforeach; ?>
 			</td>
 			<td><?php echo ($user->active) ? anchor("auth/deactivate/".$user->id, lang('index_active_link')) : anchor("auth/activate/". $user->id, lang('index_inactive_link')); ?></td>
 			<td><?php echo anchor("auth/edit_user/".$user->id, 'Edit'); ?></td>
