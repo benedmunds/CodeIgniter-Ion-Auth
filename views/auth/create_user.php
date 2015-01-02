b32a26ac88615dff0443aeca8c5062a47806240b
Tim Joosten <Topairy@gmail.com>
2015-01-02 21:19:16 +0100

    adding spaces between php code and closing tag

--- views/auth/create_user.php
+++ views/auth/create_user.php
@@ -2,42 +2,42 @@
 
 	<h1>Create User</h1>
 	<p>Please enter the users information below.</p>
-	
-	<div id="infoMessage"><?php echo $message;?></div>
-	
-    <?php echo form_open("auth/create_user");?>
-      <p>First Name:<br />
-      <?php echo form_input($first_name);?>
-      </p>
-      
-      <p>Last Name:<br />
-      <?php echo form_input($last_name);?>
-      </p>
-      
-      <p>Company Name:<br />
-      <?php echo form_input($company);?>
-      </p>
-      
-      <p>Email:<br />
-      <?php echo form_input($email);?>
-      </p>
-      
-      <p>Phone:<br />
-      <?php echo form_input($phone1);?>-<?php echo form_input($phone2);?>-<?php echo form_input($phone3);?>
-      </p>
-      
-      <p>Password:<br />
-      <?php echo form_input($password);?>
-      </p>
-      
-      <p>Confirm Password:<br />
-      <?php echo form_input($password_confirm);?>
-      </p>
-      
-      
-      <p><?php echo form_submit('submit', 'Create User');?></p>
 
-      
-    <?php echo form_close();?>
+	<div id="infoMessage"><?php echo $message; ?></div>
+
+    <?php echo form_open("auth/create_user"); ?>
+      <p>First Name:<br />
+      <?php echo form_input($first_name); ?>
+      </p>
+
+      <p>Last Name:<br />
+      <?php echo form_input($last_name); ?>
+      </p>
+
+      <p>Company Name:<br />
+      <?php echo form_input($company); ?>
+      </p>
+
+      <p>Email:<br />
+      <?php echo form_input($email); ?>
+      </p>
+
+      <p>Phone:<br />
+      <?php echo form_input($phone1); ?>-<?php echo form_input($phone2); ?>-<?php echo form_input($phone3); ?>
+      </p>
+
+      <p>Password:<br />
+      <?php echo form_input($password); ?>
+      </p>
+
+      <p>Confirm Password:<br />
+      <?php echo form_input($password_confirm); ?>
+      </p>
+
+
+      <p><?php echo form_submit('submit', 'Create User'); ?></p>
+
+
+    <?php echo form_close(); ?>
 
 </div>
--- views/auth/index.php
+++ views/auth/index.php
@@ -2,9 +2,9 @@
 
 	<h1>Users</h1>
 	<p>Below is a list of the users.</p>
-	
-	<div id="infoMessage"><?php echo $message;?></div>
-	
+
+	<div id="infoMessage"><?php echo $message; ?></div>
+
 	<table cellpadding=0 cellspacing=10>
 		<tr>
 			<th>First Name</th>
@@ -13,19 +13,19 @@
 			<th>Group</th>
 			<th>Status</th>
 		</tr>
-		<?php foreach ($users as $user):?>
+		<?php foreach ($users as $user): ?>
 			<tr>
-				<td><?php echo $user['first_name']?></td>
-				<td><?php echo $user['last_name']?></td>
-				<td><?php echo $user['email'];?></td>
-				<td><?php echo $user['group_description'];?></td>
-				<td><?php echo ($user['active']) ? anchor("auth/deactivate/".$user['id'], 'Active') : anchor("auth/activate/". $user['id'], 'Inactive');?></td>
+				<td><?php echo $user['first_name'] ?></td>
+				<td><?php echo $user['last_name'] ?></td>
+				<td><?php echo $user['email']; ?></td>
+				<td><?php echo $user['group_description']; ?></td>
+				<td><?php echo ($user['active']) ? anchor("auth/deactivate/".$user['id'], 'Active') : anchor("auth/activate/". $user['id'], 'Inactive'); ?></td>
 			</tr>
 		<?php endforeach;?>
 	</table>
-	
-	<p><a href="<?php echo site_url('auth/create_user');?>">Create a new user</a></p>
-	
+
+	<p><a href="<?php echo site_url('auth/create_user'); ?>">Create a new user</a></p>
+
 	<p><a href="<?php echo site_url('auth/logout'); ?>">Logout</a></p>
-	
+
 </div>
