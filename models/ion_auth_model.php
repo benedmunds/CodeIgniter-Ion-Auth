e01df2f376ca71a9272b490b90d2d9ed39ca2b4c
Adrian Voicu <avenir.ro@gmail.com>
2015-02-08 00:21:42 +0200

    Correction for issue #727
    Correction applied for issue #727

--- models/ion_auth_model.php
+++ models/ion_auth_model.php
@@ -891,7 +891,7 @@ class Ion_auth_model extends CI_Model
 		}
 
 		//check if the default set in config exists in database
-		$query = $this->db->get_where('groups',array('name' => $this->config->item('default_group', 'ion_auth')),1)->row();
+		$query = $this->db->get_where($this->tables['groups'],array('name' => $this->config->item('default_group', 'ion_auth')),1)->row();
 		if( !isset($query->id) && empty($groups) ) 
 		{
 			$this->set_error('account_creation_invalid_default_group');
