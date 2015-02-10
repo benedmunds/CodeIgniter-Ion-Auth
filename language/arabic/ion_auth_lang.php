a68f2eef2d94ecc2a03809fb266ae65dbf06340a
Adrian Voicu <avenir.ro@gmail.com>
2015-02-11 00:22:07 +0200

    Update ion_auth_lang.php

--- language/arabic/ion_auth_lang.php
+++ language/arabic/ion_auth_lang.php
@@ -14,34 +14,34 @@
 */
 
 // Account Creation
-$lang['account_creation_successful'] 	  	 = 'تم انشاء حسابك بنجاح';
-$lang['account_creation_unsuccessful'] 	 	 = 'حدث خطأ اثناء انشاء حسابك لدينا';
-$lang['account_creation_duplicate_email'] 	 = 'هذا البريد الإلكترونى تم استخدامه من قبل او غير صحيح';
-$lang['account_creation_duplicate_username'] 	 = 'اسم المستخدم تم التسجيل به من قبل او غير صحيح';
+$lang['account_creation_successful']            = 'تم انشاء حسابك بنجاح';
+$lang['account_creation_unsuccessful']          = 'حدث خطأ اثناء انشاء حسابك لدينا';
+$lang['account_creation_duplicate_email'] 	    = 'هذا البريد الإلكترونى تم استخدامه من قبل او غير صحيح';
+$lang['account_creation_duplicate_username']    = 'اسم المستخدم تم التسجيل به من قبل او غير صحيح';
 $lang['account_creation_missing_default_group'] = 'Default group is not set';
 $lang['account_creation_invalid_default_group'] = 'Invalid default group name set';
 
 
 // Password
-$lang['password_change_successful'] 	 	 = 'تم تغيير كلمة السر';
-$lang['password_change_unsuccessful'] 	  	 = 'لا يمكن تغيير كلمة السر';
-$lang['forgot_password_successful'] 	 	 = 'تم ارسال بريد لإستعادة كلمة السر';
-$lang['forgot_password_unsuccessful'] 	 	 = 'لا يمكن استعادة كلمة السر';
+$lang['password_change_successful']   = 'تم تغيير كلمة السر';
+$lang['password_change_unsuccessful'] = 'لا يمكن تغيير كلمة السر';
+$lang['forgot_password_successful']   = 'تم ارسال بريد لإستعادة كلمة السر';
+$lang['forgot_password_unsuccessful'] = 'لا يمكن استعادة كلمة السر';
 
 // Activation
-$lang['activate_successful'] 		  	 = 'تم تفعيل حسابك';
-$lang['activate_unsuccessful'] 		 	 = 'لا يمكن تفعيل حسابك';
-$lang['deactivate_successful'] 		  	 = 'تم إيقاف حسابك';
-$lang['deactivate_unsuccessful'] 	  	 = 'لا يمكن إيقاف حسابك';
-$lang['activation_email_successful'] 	  	 = 'تم إرسال بريد التفعيل';
-$lang['activation_email_unsuccessful']   	 = 'لا يمكن ارسال بريد التفعيل';
+$lang['activate_successful']            = 'تم تفعيل حسابك';
+$lang['activate_unsuccessful']          = 'لا يمكن تفعيل حسابك';
+$lang['deactivate_successful']          = 'تم إيقاف حسابك';
+$lang['deactivate_unsuccessful']        = 'لا يمكن إيقاف حسابك';
+$lang['activation_email_successful']    = 'تم إرسال بريد التفعيل';
+$lang['activation_email_unsuccessful']  = 'لا يمكن ارسال بريد التفعيل';
 
 // Login / Logout
-$lang['login_successful'] 		  	 = 'تم تسجيل الدخول بنجاح';
-$lang['login_unsuccessful'] 		  	 = 'معلومات الدخول غير صحيحة';
-$lang['login_unsuccessful_not_active'] 		 = 'Account is inactive';
-$lang['login_timeout']                       = 'Temporarily Locked Out.  Try again later.';
-$lang['logout_successful'] 		 	 = 'تم تسجيل خروجك';
+$lang['login_successful']             = 'تم تسجيل الدخول بنجاح';
+$lang['login_unsuccessful']           = 'معلومات الدخول غير صحيحة';
+$lang['login_unsuccessful_not_active']= 'Account is inactive';
+$lang['login_timeout']                = 'Temporarily Locked Out.  Try again later.';
+$lang['logout_successful']            = 'تم تسجيل خروجك';
   
 // Account Changes
 $lang['update_successful'] 		 	 = 'تم تعديل معلومات حسابك';
