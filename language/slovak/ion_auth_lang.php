6082cd9793177a67f4c074ec8e11fcc5a7789368
Adrian Voicu <avenir.ro@gmail.com>
2015-02-11 00:02:26 +0200

    needs translation in slovak

--- language/slovak/ion_auth_lang.php
+++ language/slovak/ion_auth_lang.php
@@ -40,6 +40,7 @@ $lang['activation_email_unsuccessful']   	 = 'Nedá sa odoslať aktivačný e-ma
 $lang['login_successful'] 		  	         = 'Úspešne prihlásený';
 $lang['login_unsuccessful'] 		  	     = 'Nesprávny e-mail alebo heslo';
 $lang['login_unsuccessful_not_active'] 		 = 'Účet je neaktívny';
+$lang['login_timeout']                       = 'Temporarily Locked Out.  Try again later.';
 $lang['logout_successful'] 		 	         = 'Úspešné odhlásenie';
 
 // Account Changes
@@ -48,7 +49,25 @@ $lang['update_unsuccessful'] 		 	     = 'Informácie o účte sa nedájú aktual
 $lang['delete_successful'] 		 	         = 'Užívateľ bol zmazaný';
 $lang['delete_unsuccessful'] 		 	     = 'Užívateľ sa nedá zmazať ';
 
-// Email Subjects - TODO Please Translate
-$lang['email_forgotten_password_subject']    = 'Forgotten Password Verification';
-$lang['email_new_password_subject']          = 'New Password';
+// Groups
+$lang['group_creation_successful']  = 'Group created Successfully';
+$lang['group_already_exists']       = 'Group name already taken';
+$lang['group_update_successful']    = 'Group details updated';
+$lang['group_delete_successful']    = 'Group deleted';
+$lang['group_delete_unsuccessful'] 	= 'Unable to delete group';
+$lang['group_delete_notallowed']    = 'Can\'t delete the administrators\' group';
+$lang['group_name_required'] 		= 'Group name is a required field';
+// Activation Email
 $lang['email_activation_subject']            = 'Account Activation';
+$lang['email_activate_heading']    = 'Activate account for %s';
+$lang['email_activate_subheading'] = 'Please click this link to %s.';
+$lang['email_activate_link']       = 'Activate Your Account';
+// Forgot Password Email
+$lang['email_forgotten_password_subject']    = 'Forgotten Password Verification';
+$lang['email_forgot_password_heading']    = 'Reset Password for %s';
+$lang['email_forgot_password_subheading'] = 'Please click this link to %s.';
+$lang['email_forgot_password_link']       = 'Reset Your Password';
+// New Password Email
+$lang['email_new_password_subject']          = 'New Password';
+$lang['email_new_password_heading']    = 'New Password for %s';
+$lang['email_new_password_subheading'] = 'Your password has been reset to: %s';
