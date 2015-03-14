b78e07f1f01c5cd4d043907c26602a123fd2fd49
ozdemirbur <mail@burakozdemir.co.uk>
2015-03-14 13:10:06 +0200

    updated reset password method on success

--- controllers/auth.php
+++ controllers/auth.php
@@ -322,7 +322,7 @@ class Auth extends CI_Controller {
 					{
 						//if the password was successfully changed
 						$this->session->set_flashdata('message', $this->ion_auth->messages());
-						$this->logout();
+						redirect("auth/login", 'refresh');
 					}
 					else
 					{
