dd2285281b0029a1847855e54b602c1b2e4ad51e
Suhindra <suhindra@hotmail.co.id>
2015-06-28 02:43:11 +0700

    Update ion_auth_lang.php

--- language/indonesian/ion_auth_lang.php
+++ language/indonesian/ion_auth_lang.php
@@ -17,17 +17,17 @@
 * Location: https://github.com/benedmunds/CodeIgniter-Ion-Auth
 *
 * Created:  11.15.2011
-* Last-Edited:   June 27th 2015
+* Last-Edit:   June 27th 2015
 *
 * Description:  Indonesian language file for Ion Auth messages and errors
 *
 */
 
 // Account Creation
-$lang['account_creation_successful']				= 'Akun Berhasil Dibuat';
+$lang['account_creation_successful']				  = 'Akun Berhasil Dibuat';
 $lang['account_creation_unsuccessful']				= 'Tidak Dapat Membuat Akun';
 $lang['account_creation_duplicate_email']			= 'Email Sudah Digunakan atau Tidak Valid';
-$lang['account_creation_duplicate_username']		= 'Username Sudah Digunakan atau Tidak Valid';
+$lang['account_creation_duplicate_username']	= 'Username Sudah Digunakan atau Tidak Valid';
 
 // TODO Please Translate
 $lang['account_creation_missing_default_group']		= 'Standar grup tidak diatur';
@@ -41,18 +41,18 @@ $lang['forgot_password_successful']					= 'Email untuk Set Ulang Kata Sandi Tela
 $lang['forgot_password_unsuccessful']				= 'Tidak Dapat Set Ulang Kata Sandi';
 
 // Activation
-$lang['activate_successful']						= 'Akun Telah Diaktifkan';
+$lang['activate_successful']						  = 'Akun Telah Diaktifkan';
 $lang['activate_unsuccessful']						= 'Tidak Dapat Mengaktifkan Akun';
 $lang['deactivate_successful']						= 'Akun Telah Dinonaktifkan';
 $lang['deactivate_unsuccessful']					= 'Tidak Dapat Menonaktifkan Akun';
-$lang['activation_email_successful']				= 'Email untuk Aktivasi Telah Dikirim';
-$lang['activation_email_unsuccessful']				= 'Tidak Dapat Mengirimkan Email Aktivasi';
+$lang['activation_email_successful']			= 'Email untuk Aktivasi Telah Dikirim';
+$lang['activation_email_unsuccessful']		= 'Tidak Dapat Mengirimkan Email Aktivasi';
 
 // Login / Logout
-$lang['login_successful']							= 'Log In Berhasil';
+$lang['login_successful']							  = 'Log In Berhasil';
 $lang['login_unsuccessful']							= 'Log In Gagal';
-$lang['login_unsuccessful_not_active']				= 'Akun Tidak Aktif';
-$lang['login_timeout']								= 'Sementara Terkunci. Coba Beberapa Saat Lagi.';
+$lang['login_unsuccessful_not_active']	= 'Akun Tidak Aktif';
+$lang['login_timeout']								  = 'Sementara Terkunci. Coba Beberapa Saat Lagi.';
 $lang['logout_successful']							= 'Log Out Berhasil';
 
 // Account Changes
@@ -62,28 +62,28 @@ $lang['delete_successful']							= 'Pengguna Telah Dihapus';
 $lang['delete_unsuccessful']						= 'Gagal Menghapus Pengguna';
 
 // Groups
-$lang['group_creation_successful']					= 'Grup Berhasil Dibuat';
-$lang['group_already_exists']						= 'Nama Grup Sudah Digunakan';
+$lang['group_creation_successful']				= 'Grup Berhasil Dibuat';
+$lang['group_already_exists']						  = 'Nama Grup Sudah Digunakan';
 $lang['group_update_successful']					= 'Detil Grup Berhasil Diubah';
 $lang['group_delete_successful']					= 'Grup Berhasil Dihapus';
-$lang['group_delete_unsuccessful']					= 'Gagal Menghapus Grup';
+$lang['group_delete_unsuccessful']				= 'Gagal Menghapus Grup';
 $lang['group_delete_notallowed']					= 'Tidak Dapat menghapus Grup Administrator';
-$lang['group_name_required']						= 'Nama Grup Tidak Boleh Kosong';
-$lang['group_name_admin_not_alter']					= 'Nama Grup Admin Tidak Bisa Diubah';
+$lang['group_name_required']						  = 'Nama Grup Tidak Boleh Kosong';
+$lang['group_name_admin_not_alter']				= 'Nama Grup Admin Tidak Bisa Diubah';
 
 // Activation Email
 $lang['email_activation_subject']					= 'Aktivasi Akun';
 $lang['email_activate_heading']						= 'Aktifkan Akun dari %s';
-$lang['email_activate_subheading']					= 'Silahkan klik tautan berikut untuk %s.';
-$lang['email_activate_link']						= 'Aktifkan Akun';
+$lang['email_activate_subheading']				= 'Silahkan klik tautan berikut untuk %s.';
+$lang['email_activate_link']						  = 'Aktifkan Akun';
 
 // Forgot Password Email
 $lang['email_forgotten_password_subject']			= 'Verifikasi Lupa Password';
 $lang['email_forgot_password_heading']				= 'Setel Ulang Kata Sandi untuk %s';
 $lang['email_forgot_password_subheading']			= 'Silahkan klik tautan berikut untuk %s.';
-$lang['email_forgot_password_link']					= 'Setel Ulang Kata Sandi';
+$lang['email_forgot_password_link']					  = 'Setel Ulang Kata Sandi';
 
 // New Password Email
 $lang['email_new_password_subject']					= 'Kata Sandi Baru';
 $lang['email_new_password_heading']					= 'Kata Sandi Baru Untuk %s';
-$lang['email_new_password_subheading']				= 'Kata Sandi Telah Diubah Ke: %s';
+$lang['email_new_password_subheading']			= 'Kata Sandi Telah Diubah Ke: %s';
