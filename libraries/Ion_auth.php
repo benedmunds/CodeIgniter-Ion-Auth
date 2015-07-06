90c65d30c3be35cc36a86272dd90af599cddb23e
sigmoidal <stathis@npcglib.org>
2015-07-06 19:24:23 +0300

    Update Ion_auth.php

--- libraries/Ion_auth.php
+++ libraries/Ion_auth.php
@@ -386,8 +386,15 @@ class Ion_auth
 
 		$identity = $this->config->item('identity', 'ion_auth');
                 
-                $this->session->unset_userdata( array($identity, 'id', 'user_id') );
-
+                if (substr(CI_VERSION, 0, 1) == '2')
+		{
+			$this->session->unset_userdata( array($identity => '', 'id' => '', 'user_id' => '') );
+                } 
+                else 
+                {
+                	$this->session->unset_userdata( array($identity, 'id', 'user_id') );
+                }
+                
 		//delete the remember me cookies if they exist
 		if (get_cookie($this->config->item('identity_cookie_name', 'ion_auth')))
 		{
