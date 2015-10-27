160849cb2455fdff80e3140fcfcf5fca5bbff290
KasperGreen <kaspergreen@gmail.com>
2015-10-27 05:40:57 +0300

    Update ion_auth_lang.php

--- language/russian/ion_auth_lang.php
+++ language/russian/ion_auth_lang.php
@@ -20,9 +20,7 @@
 $lang['account_creation_successful'] 	  	 = 'Учетная запись успешно создана';
 $lang['account_creation_unsuccessful'] 	 	 = 'Невозможно создать учетную запись';
 $lang['account_creation_duplicate_email'] 	 = 'Электронная почта используется или некорректна';
-$lang['account_creation_duplicate_identity'] 	 = 'Имя пользователя существует или некорректно';
-
-// TODO Please Translate
+$lang['account_creation_duplicate_username'] 	 = 'Имя пользователя существует или некорректно';
 $lang['account_creation_missing_default_group'] = 'Группа по умолчанию не установлена';
 $lang['account_creation_invalid_default_group'] = 'Группа по умолчанию задана некорректно';
 
@@ -43,8 +41,8 @@ $lang['activation_email_unsuccessful']   	 = 'Сообщение об актив
 // Login / Logout
 $lang['login_successful'] 		  	 = 'Авторизация прошла успешно';
 $lang['login_unsuccessful'] 		  	 = 'Логин/пароль не верен';
-$lang['login_unsuccessful_not_active'] 		 = 'Account is inactive';
-$lang['login_timeout']                       = 'Temporarily Locked Out.  Try again later.';
+$lang['login_unsuccessful_not_active'] 		 = 'Акаунт не активен';
+$lang['login_timeout']                       = 'В целях безопасности возможность входа временно заблокирована. Попробуйте зайти позже.';
 $lang['logout_successful'] 		 	 = 'Выход успешный';
 
 // Account Changes
@@ -54,26 +52,24 @@ $lang['delete_successful'] 		 	 = 'Учетная запись удалена';
 $lang['delete_unsuccessful'] 		 	 = 'Невозможно удалить учетную запись';
 
 // Groups
-$lang['group_creation_successful']  = 'Group created Successfully';
-$lang['group_already_exists']       = 'Group name already taken';
-$lang['group_update_successful']    = 'Group details updated';
-$lang['group_delete_successful']    = 'Group deleted';
-$lang['group_delete_unsuccessful'] 	= 'Unable to delete group';
-$lang['group_delete_notallowed']    = 'Can\'t delete the administrators\' group';
-$lang['group_name_required'] 		= 'Group name is a required field';
-$lang['group_name_admin_not_alter'] = 'Admin group name can not be changed';
-
+$lang['group_creation_successful']  = 'Группа создана успешно';
+$lang['group_already_exists']       = 'Группа с таким именем уже существует';
+$lang['group_update_successful']    = 'Данные группы обновлены успешно';
+$lang['group_delete_successful']    = 'Группа удалена';
+$lang['group_delete_unsuccessful'] 	= 'Не удалось удалить группу';
+$lang['group_delete_notallowed']    = 'Нельзя удалить группу администраторов';
+$lang['group_name_required'] 		= 'Имя группы обязательно к заполнению';
 // Activation Email
 $lang['email_activation_subject']            = 'Активация учетной записи';
-$lang['email_activate_heading']    = 'Activate account for %s';
-$lang['email_activate_subheading'] = 'Please click this link to %s.';
-$lang['email_activate_link']       = 'Activate Your Account';
+$lang['email_activate_heading']    = 'Активировать акаунт с именем  %s';
+$lang['email_activate_subheading'] = 'Нажмите на ссылку %s.';
+$lang['email_activate_link']       = 'Активировать ваш акаунт';
 // Forgot Password Email
 $lang['email_forgotten_password_subject']    = 'Проверка забытого пароля';
-$lang['email_forgot_password_heading']    = 'Reset Password for %s';
-$lang['email_forgot_password_subheading'] = 'Please click this link to %s.';
-$lang['email_forgot_password_link']       = 'Reset Your Password';
+$lang['email_forgot_password_heading']    = 'Сброс пароля для пользователя %s';
+$lang['email_forgot_password_subheading'] = 'Нажмите на ссылку для %s.';
+$lang['email_forgot_password_link']       = 'Восстановления пароля';
 // New Password Email
 $lang['email_new_password_subject']          = 'Новый пароль';
-$lang['email_new_password_heading']    = 'New Password for %s';
-$lang['email_new_password_subheading'] = 'Your password has been reset to: %s';
+$lang['email_new_password_heading']    = 'Новый пароль для %s';
+$lang['email_new_password_subheading'] = 'Пароль изменён на: %s';
