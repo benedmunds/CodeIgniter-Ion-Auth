76e0bb22e43420d1e4ab8e49373e5c76cef7dbb7
Rajitha Bandara <rcb44u@gmail.com>
2016-06-17 22:23:04 +0530

    Updated Sinhala translations

--- language/sinhala/auth_lang.php
+++ language/sinhala/auth_lang.php
@@ -18,127 +18,127 @@
 */
 
 // Errors
-$lang['error_csrf'] = 'This form post did not pass our security checks.';
+$lang['error_csrf'] = 'මෙම පෝරමය ආරක්ෂිත පිරික්සුම් සදහා අනුගත නැත.';
 
 // Login
-$lang['login_heading']         = 'Login';
-$lang['login_subheading']      = 'Please login with your email/username and password below.';
-$lang['login_identity_label']  = 'Email/Username:';
-$lang['login_password_label']  = 'Password:';
-$lang['login_remember_label']  = 'Remember Me:';
-$lang['login_submit_btn']      = 'Login';
-$lang['login_forgot_password'] = 'Forgot your password?';
+$lang['login_heading']         = 'ඇතුලු වීම';
+$lang['login_subheading']      = 'කරුණාකර පහත ඔබගේ ඊමේල්/පරිශීලක නාමය හා මුර පදය යොදා ඇතුලු වන්න.';
+$lang['login_identity_label']  = 'ඊමේල්/පරිශීලක නාමය:';
+$lang['login_password_label']  = 'මුර පදය:';
+$lang['login_remember_label']  = 'මතක තබාගන්න:';
+$lang['login_submit_btn']      = 'ඇතුලු වීම';
+$lang['login_forgot_password'] = 'මුර පදය අමතකයි ද?';
 
 // Index
-$lang['index_heading']           = 'Users';
-$lang['index_subheading']        = 'Below is a list of the users.';
-$lang['index_fname_th']          = 'First Name';
-$lang['index_lname_th']          = 'Last Name';
-$lang['index_email_th']          = 'Email';
-$lang['index_groups_th']         = 'Groups';
-$lang['index_status_th']         = 'Status';
-$lang['index_action_th']         = 'Action';
-$lang['index_active_link']       = 'Active';
-$lang['index_inactive_link']     = 'Inactive';
-$lang['index_create_user_link']  = 'Create a new user';
-$lang['index_create_group_link'] = 'Create a new group';
+$lang['index_heading']           = 'පරිශීලකයින්';
+$lang['index_subheading']        = 'පරිශීලක ලැයිස්තුව පහත ඇත.';
+$lang['index_fname_th']          = 'මුල් නම';
+$lang['index_lname_th']          = 'අග නම';
+$lang['index_email_th']          = 'ඊමේල්';
+$lang['index_groups_th']         = 'සමූහ';
+$lang['index_status_th']         = 'භාවය';
+$lang['index_action_th']         = 'ක්‍රියාව';
+$lang['index_active_link']       = 'සක්‍රීය';
+$lang['index_inactive_link']     = 'අක්‍රීය';
+$lang['index_create_user_link']  = 'නව පරිශීලකයෙක් සාදන්න';
+$lang['index_create_group_link'] = 'නව සමූහයක් සාදන්න';
 
 // Deactivate User
-$lang['deactivate_heading']                  = 'Deactivate User';
-$lang['deactivate_subheading']               = 'Are you sure you want to deactivate the user \'%s\'';
-$lang['deactivate_confirm_y_label']          = 'Yes:';
-$lang['deactivate_confirm_n_label']          = 'No:';
-$lang['deactivate_submit_btn']               = 'Submit';
-$lang['deactivate_validation_confirm_label'] = 'confirmation';
-$lang['deactivate_validation_user_id_label'] = 'user ID';
+$lang['deactivate_heading']                  = 'පරිශීලක අක්‍රීය කරන්න';
+$lang['deactivate_subheading']               = 'මෙම පරිශීලක අක්‍රීය කිරීම විශ්වාසයි \'%s\'';
+$lang['deactivate_confirm_y_label']          = 'ඔව්:';
+$lang['deactivate_confirm_n_label']          = 'නැත:';
+$lang['deactivate_submit_btn']               = 'යොමු කරන්න';
+$lang['deactivate_validation_confirm_label'] = 'තහවුරු කිරීම';
+$lang['deactivate_validation_user_id_label'] = 'පරිශීලක හැඳුනුම';
 
 // Create User
-$lang['create_user_heading']                           = 'Create User';
-$lang['create_user_subheading']                        = 'Please enter the user\'s information below.';
-$lang['create_user_fname_label']                       = 'First Name:';
-$lang['create_user_lname_label']                       = 'Last Name:';
-$lang['create_user_company_label']                     = 'Company Name:';
-$lang['create_user_identity_label']                    = 'Identity:';
-$lang['create_user_email_label']                       = 'Email:';
-$lang['create_user_phone_label']                       = 'Phone:';
-$lang['create_user_password_label']                    = 'Password:';
-$lang['create_user_password_confirm_label']            = 'Confirm Password:';
-$lang['create_user_submit_btn']                        = 'Create User';
-$lang['create_user_validation_fname_label']            = 'First Name';
-$lang['create_user_validation_lname_label']            = 'Last Name';
-$lang['create_user_validation_identity_label']         = 'Identity';
-$lang['create_user_validation_email_label']            = 'Email Address';
-$lang['create_user_validation_phone_label']            = 'Phone';
-$lang['create_user_validation_company_label']          = 'Company Name';
-$lang['create_user_validation_password_label']         = 'Password';
-$lang['create_user_validation_password_confirm_label'] = 'Password Confirmation';
+$lang['create_user_heading']                           = 'පරිශීලක සාදන්න';
+$lang['create_user_subheading']                        = 'කරුණාකර පරිශීලක තොරතුරු පහත ඇතුලත් කරන්න.';
+$lang['create_user_fname_label']                       = 'මුල් නම:';
+$lang['create_user_lname_label']                       = 'අග නම:';
+$lang['create_user_company_label']                     = 'සමාගම් නාමය:';
+$lang['create_user_identity_label']                    = 'අනන්‍යතාව:';
+$lang['create_user_email_label']                       = 'ඊමේල්:';
+$lang['create_user_phone_label']                       = 'දුරකතන:';
+$lang['create_user_password_label']                    = 'මුර පදය:';
+$lang['create_user_password_confirm_label']            = 'මුර පදය තහවුරු:';
+$lang['create_user_submit_btn']                        = 'පරිශීලක සාදන්න';
+$lang['create_user_validation_fname_label']            = 'මුල් නම';
+$lang['create_user_validation_lname_label']            = 'අග නම';
+$lang['create_user_validation_identity_label']         = 'අනන්‍යතාව';
+$lang['create_user_validation_email_label']            = 'ඊමේල් ලිපිනය';
+$lang['create_user_validation_phone_label']            = 'දුරකතන';
+$lang['create_user_validation_company_label']          = 'සමාගම් නාමය';
+$lang['create_user_validation_password_label']         = 'මුර පදය';
+$lang['create_user_validation_password_confirm_label'] = 'මුර පදය තහවුරු';
 
 // Edit User
-$lang['edit_user_heading']                           = 'Edit User';
-$lang['edit_user_subheading']                        = 'Please enter the user\'s information below.';
-$lang['edit_user_fname_label']                       = 'First Name:';
-$lang['edit_user_lname_label']                       = 'Last Name:';
-$lang['edit_user_company_label']                     = 'Company Name:';
-$lang['edit_user_email_label']                       = 'Email:';
-$lang['edit_user_phone_label']                       = 'Phone:';
-$lang['edit_user_password_label']                    = 'Password: (if changing password)';
-$lang['edit_user_password_confirm_label']            = 'Confirm Password: (if changing password)';
-$lang['edit_user_groups_heading']                    = 'Member of groups';
-$lang['edit_user_submit_btn']                        = 'Save User';
-$lang['edit_user_validation_fname_label']            = 'First Name';
-$lang['edit_user_validation_lname_label']            = 'Last Name';
-$lang['edit_user_validation_email_label']            = 'Email Address';
-$lang['edit_user_validation_phone_label']            = 'Phone';
-$lang['edit_user_validation_company_label']          = 'Company Name';
-$lang['edit_user_validation_groups_label']           = 'Groups';
-$lang['edit_user_validation_password_label']         = 'Password';
-$lang['edit_user_validation_password_confirm_label'] = 'Password Confirmation';
+$lang['edit_user_heading']                           = 'පරිශීලක සංස්කරණය';
+$lang['edit_user_subheading']                        = 'කරුණාකර පරිශීලක තොරතුරු පහත ඇතුලත් කරන්න.';
+$lang['edit_user_fname_label']                       = 'මුල් නම:';
+$lang['edit_user_lname_label']                       = 'අග නම:';
+$lang['edit_user_company_label']                     = 'සමාගම් නාමය:';
+$lang['edit_user_email_label']                       = 'ඊමේල්:';
+$lang['edit_user_phone_label']                       = 'දුරකතන:';
+$lang['edit_user_password_label']                    = 'මුර පදය: (මුර පදය වෙනස් වේ නම්)';
+$lang['edit_user_password_confirm_label']            = 'මුර පදය තහවුරු: (මුර පදය වෙනස් වේ නම්)';
+$lang['edit_user_groups_heading']                    = 'සමූහ සාමාජික';
+$lang['edit_user_submit_btn']                        = 'පරිශීලක සුරැකුම්';
+$lang['edit_user_validation_fname_label']            = 'මුල් නම';
+$lang['edit_user_validation_lname_label']            = 'අග නම';
+$lang['edit_user_validation_email_label']            = 'ඊමේල් ලිපිනය';
+$lang['edit_user_validation_phone_label']            = 'දුරකතන';
+$lang['edit_user_validation_company_label']          = 'සමාගම් නාමය';
+$lang['edit_user_validation_groups_label']           = 'සමූහ';
+$lang['edit_user_validation_password_label']         = 'මුර පදය';
+$lang['edit_user_validation_password_confirm_label'] = 'මුර පදය තහවුරු';
 
 // Create Group
-$lang['create_group_title']                  = 'Create Group';
-$lang['create_group_heading']                = 'Create Group';
-$lang['create_group_subheading']             = 'Please enter the group information below.';
-$lang['create_group_name_label']             = 'Group Name:';
-$lang['create_group_desc_label']             = 'Description:';
-$lang['create_group_submit_btn']             = 'Create Group';
-$lang['create_group_validation_name_label']  = 'Group Name';
-$lang['create_group_validation_desc_label']  = 'Description';
+$lang['create_group_title']                  = 'සමූහය සාදන්න';
+$lang['create_group_heading']                = 'සමූහය සාදන්න';
+$lang['create_group_subheading']             = 'කරුණාකර සමූහයේ තොරතුරු පහත ඇතුලත කරන්න.';
+$lang['create_group_name_label']             = 'සමූහ නාමය:';
+$lang['create_group_desc_label']             = 'විස්තර:';
+$lang['create_group_submit_btn']             = 'සමූහය සාදන්න';
+$lang['create_group_validation_name_label']  = 'සමූහ නාමය';
+$lang['create_group_validation_desc_label']  = 'විස්තර';
 
 // Edit Group
-$lang['edit_group_title']                  = 'Edit Group';
-$lang['edit_group_saved']                  = 'Group Saved';
-$lang['edit_group_heading']                = 'Edit Group';
-$lang['edit_group_subheading']             = 'Please enter the group information below.';
-$lang['edit_group_name_label']             = 'Group Name:';
-$lang['edit_group_desc_label']             = 'Description:';
-$lang['edit_group_submit_btn']             = 'Save Group';
-$lang['edit_group_validation_name_label']  = 'Group Name';
-$lang['edit_group_validation_desc_label']  = 'Description';
+$lang['edit_group_title']                  = 'සමූහය සංස්කරණය';
+$lang['edit_group_saved']                  = 'සමූහය සුරැකි';
+$lang['edit_group_heading']                = 'සමූහය සංස්කරණය';
+$lang['edit_group_subheading']             = 'කරුණාකර සමූහයේ තොරතුරු පහත ඇතුලත කරන්න.';
+$lang['edit_group_name_label']             = 'සමූහ නාමය:';
+$lang['edit_group_desc_label']             = 'විස්තර:';
+$lang['edit_group_submit_btn']             = 'සමූහය සුරකින්න';
+$lang['edit_group_validation_name_label']  = 'සමූහ නාමය';
+$lang['edit_group_validation_desc_label']  = 'විස්තර';
 
 // Change Password
-$lang['change_password_heading']                               = 'Change Password';
-$lang['change_password_old_password_label']                    = 'Old Password:';
-$lang['change_password_new_password_label']                    = 'New Password (at least %s characters long):';
-$lang['change_password_new_password_confirm_label']            = 'Confirm New Password:';
-$lang['change_password_submit_btn']                            = 'Change';
-$lang['change_password_validation_old_password_label']         = 'Old Password';
-$lang['change_password_validation_new_password_label']         = 'New Password';
-$lang['change_password_validation_new_password_confirm_label'] = 'Confirm New Password';
+$lang['change_password_heading']                               = 'මුර පදය වෙනස් කරන්න';
+$lang['change_password_old_password_label']                    = 'පෙර මුර පදය:';
+$lang['change_password_new_password_label']                    = 'නව මුර පදය (අවම අක්ෂර ගණන %s):';
+$lang['change_password_new_password_confirm_label']            = 'නව මුර පදය තහවුරු කරන්න:';
+$lang['change_password_submit_btn']                            = 'වෙනස් කරන්න';
+$lang['change_password_validation_old_password_label']         = 'පෙර මුර පදය';
+$lang['change_password_validation_new_password_label']         = 'නව මුර පදය';
+$lang['change_password_validation_new_password_confirm_label'] = 'නව මුර පදය තහවුරු';
 
 // Forgot Password
-$lang['forgot_password_heading']                 = 'Forgot Password';
-$lang['forgot_password_subheading']              = 'Please enter your %s so we can send you an email to reset your password.';
+$lang['forgot_password_heading']                 = 'මුර පදය අමතකයි';
+$lang['forgot_password_subheading']              = 'මුර පදය නැවත සැකසීමට ඊමේලයක් යැවීම සදහා කරුණාකර ඔබගේ %s  ඇතුලත් කරන්න.';
 $lang['forgot_password_email_label']             = '%s:';
-$lang['forgot_password_submit_btn']              = 'Submit';
-$lang['forgot_password_validation_email_label']  = 'Email Address';
-$lang['forgot_password_identity_label'] = 'Identity';
-$lang['forgot_password_email_identity_label']    = 'Email';
-$lang['forgot_password_email_not_found']         = 'No record of that email address.';
+$lang['forgot_password_submit_btn']              = 'යොමු කරන්න';
+$lang['forgot_password_validation_email_label']  = 'ඊමේල් ලිපිනය';
+$lang['forgot_password_identity_label'] = 'අනන්‍යතාව';
+$lang['forgot_password_email_identity_label']    = 'ඊමේල්';
+$lang['forgot_password_email_not_found']         = 'ඊමේල් ලිපිනය සොයාගත නොහැක.';
 
 // Reset Password
-$lang['reset_password_heading']                               = 'Change Password';
-$lang['reset_password_new_password_label']                    = 'New Password (at least %s characters long):';
-$lang['reset_password_new_password_confirm_label']            = 'Confirm New Password:';
-$lang['reset_password_submit_btn']                            = 'Change';
-$lang['reset_password_validation_new_password_label']         = 'New Password';
-$lang['reset_password_validation_new_password_confirm_label'] = 'Confirm New Password';
+$lang['reset_password_heading']                               = 'මුර පදය වෙනස් කරන්න';
+$lang['reset_password_new_password_label']                    = 'නව මුර පදය (අවම අක්ෂර ගණන %s):';
+$lang['reset_password_new_password_confirm_label']            = 'නව මුර පදය තහවුරු:';
+$lang['reset_password_submit_btn']                            = 'වෙනස් කරන්න';
+$lang['reset_password_validation_new_password_label']         = 'නව මුර පදය';
+$lang['reset_password_validation_new_password_confirm_label'] = 'නව මුර තහවුරු  කරන්න';
--- language/sinhala/ion_auth_lang.php
+++ language/sinhala/ion_auth_lang.php
@@ -17,62 +17,62 @@
 // Account Creation
 $lang['account_creation_successful']            = 'ගිණුම සැකසුම සාර්ථකයි';
 $lang['account_creation_unsuccessful']          = 'ගිණුම සැකසීමට නොහැක';
-$lang['account_creation_duplicate_email']       = 'Email Already Used or Invalid';
-$lang['account_creation_duplicate_identity']    = 'Identity Already Used or Invalid';
-$lang['account_creation_missing_default_group'] = 'Default group is not set';
-$lang['account_creation_invalid_default_group'] = 'Invalid default group name set';
+$lang['account_creation_duplicate_email']       = 'ඊමේල් ලිපිනය දැනටමත් භාවිතයේ හෝ අවලංගුයි';
+$lang['account_creation_duplicate_identity']    = 'අනන්‍යතාව දැනටමත් භාවිතයේ හෝ අවලංගුයි';
+$lang['account_creation_missing_default_group'] = 'සම්මත සමූහය සකසා නැත';
+$lang['account_creation_invalid_default_group'] = 'අවලංගු සම්මත සමූහ නාමයකි';
 
 
 // Password
-$lang['password_change_successful']          = 'Password Successfully Changed';
-$lang['password_change_unsuccessful']        = 'Unable to Change Password';
-$lang['forgot_password_successful']          = 'Password Reset Email Sent';
-$lang['forgot_password_unsuccessful']        = 'Unable to Reset Password';
+$lang['password_change_successful']          = 'මුර පදය වෙනස් කිරීම සාර්ථකයි';
+$lang['password_change_unsuccessful']        = 'මුර පදය වෙනස් කල නොහැක';
+$lang['forgot_password_successful']          = 'මුර පදය නැවත සැකසුම් ඊමේලය යවා ඇත';
+$lang['forgot_password_unsuccessful']        = 'මුර පදය නැවත සැකසීමට නොහැක';
 
 // Activation
-$lang['activate_successful']                 = 'Account Activated';
-$lang['activate_unsuccessful']               = 'Unable to Activate Account';
-$lang['deactivate_successful']               = 'Account De-Activated';
-$lang['deactivate_unsuccessful']             = 'Unable to De-Activate Account';
-$lang['activation_email_successful']         = 'Activation Email Sent';
-$lang['activation_email_unsuccessful']       = 'Unable to Send Activation Email';
+$lang['activate_successful']                 = 'ගිණුම සක්‍රීය කර ඇත';
+$lang['activate_unsuccessful']               = 'ගිණුම සක්‍රීය කල නොහැක';
+$lang['deactivate_successful']               = 'ගිණුම අක්‍රීය කර ඇත';
+$lang['deactivate_unsuccessful']             = 'ගිණුම අක්‍රීය කල නොහැක';
+$lang['activation_email_successful']         = 'ගිණුම සක්‍රීය ඊමේලය යවා ඇත';
+$lang['activation_email_unsuccessful']       = 'ගිණුම සක්‍රීය ඊමේලය යැවීමට නොහැක';
 
 // Login / Logout
-$lang['login_successful']                    = 'Logged In Successfully';
-$lang['login_unsuccessful']                  = 'Incorrect Login';
-$lang['login_unsuccessful_not_active']       = 'Account is inactive';
-$lang['login_timeout']                       = 'Temporarily Locked Out.  Try again later.';
-$lang['logout_successful']                   = 'Logged Out Successfully';
+$lang['login_successful']                    = 'ඇතුලු වීම සාර්ථකයි';
+$lang['login_unsuccessful']                  = 'ඇතුලු වීම වැරදි';
+$lang['login_unsuccessful_not_active']       = 'ගිණුම සක්‍රීය නැත';
+$lang['login_timeout']                       = 'ඇතුලු කාලය ඉක්මවා ඇත. නැවත උත්සාහ කරන්න.';
+$lang['logout_successful']                   = 'පිටවීම සාර්ථකයි';
 
 // Account Changes
-$lang['update_successful']                   = 'Account Information Successfully Updated';
-$lang['update_unsuccessful']                 = 'Unable to Update Account Information';
-$lang['delete_successful']                   = 'User Deleted';
-$lang['delete_unsuccessful']                 = 'Unable to Delete User';
+$lang['update_successful']                   = 'ගිණුම් තොරතුරු සාර්ථකව යාවත්කාලීන කර ඇත';
+$lang['update_unsuccessful']                 = 'ගිණුම් තොරතුරු යාවත්කාලීන කල නොහැක';
+$lang['delete_successful']                   = 'පරිශීලක ඉවත් කර ඇත';
+$lang['delete_unsuccessful']                 = 'පරිශීලක ඉවත් කල නොහැක';
 
 // Groups
-$lang['group_creation_successful']           = 'Group created Successfully';
-$lang['group_already_exists']                = 'Group name already taken';
-$lang['group_update_successful']             = 'Group details updated';
-$lang['group_delete_successful']             = 'Group deleted';
-$lang['group_delete_unsuccessful']           = 'Unable to delete group';
-$lang['group_delete_notallowed']             = 'Can\'t delete the administrators\' group';
-$lang['group_name_required']                 = 'Group name is a required field';
-$lang['group_name_admin_not_alter']          = 'Admin group name can not be changed';
+$lang['group_creation_successful']           = 'සමූහය සාර්ථකව සකස් කර ඇත';
+$lang['group_already_exists']                = 'සමූහය නාමය දැනටමත් භාවිතයේ පවතී';
+$lang['group_update_successful']             = 'සමූහ තොරතුරු යාවත්කලීන කර ඇත';
+$lang['group_delete_successful']             = 'සමූහය ඉවත් කර ඇත';
+$lang['group_delete_unsuccessful']           = 'සමූහය ඉවත් කල නොහැක';
+$lang['group_delete_notallowed']             = 'පාලක සමූහය ඉවත් කල නොහැක';
+$lang['group_name_required']                 = 'සමූහ නාමය අනිවාර්යයි';
+$lang['group_name_admin_not_alter']          = 'පාලක සමූහ නාමය වෙනස් කල නොහැක';
 
 // Activation Email
-$lang['email_activation_subject']            = 'Account Activation';
-$lang['email_activate_heading']              = 'Activate account for %s';
-$lang['email_activate_subheading']           = 'Please click this link to %s.';
-$lang['email_activate_link']                 = 'Activate Your Account';
+$lang['email_activation_subject']            = 'ගිණුම සක්‍රීය කිරීම';
+$lang['email_activate_heading']              = '%s සදහා ගිණුම සක්‍රීය කරන්න';
+$lang['email_activate_subheading']           = 'කරුණාකර මෙම යොමුව මත ක්ලික් කරන්න %s.';
+$lang['email_activate_link']                 = 'ඔබගේ ගිණුම සක්‍රීය කරන්න';
 
 // Forgot Password Email
-$lang['email_forgotten_password_subject']    = 'Forgotten Password Verification';
-$lang['email_forgot_password_heading']       = 'Reset Password for %s';
-$lang['email_forgot_password_subheading']    = 'Please click this link to %s.';
-$lang['email_forgot_password_link']          = 'Reset Your Password';
+$lang['email_forgotten_password_subject']    = 'අමතක වූ මුර පදය  තහවුරු කිරීම';
+$lang['email_forgot_password_heading']       = '%s සදහා නැවත මුර පදය සැකසුම';
+$lang['email_forgot_password_subheading']    = 'කරුණාකර මෙම යොමුව මත ක්ලික් කරන්න %s.';
+$lang['email_forgot_password_link']          = 'ඔබගේ මුර පදය නැවත සැකසුම';
 
 // New Password Email
-$lang['email_new_password_subject']          = 'New Password';
-$lang['email_new_password_heading']          = 'New Password for %s';
-$lang['email_new_password_subheading']       = 'Your password has been reset to: %s';
+$lang['email_new_password_subject']          = 'නව මුර පදය';
+$lang['email_new_password_heading']          = '%s සදහා නව මුර පදය';
+$lang['email_new_password_subheading']       = 'ඔබගේ මුර පදය නැවත සකස් කර ඇත: %s';
