9d60fcf7d365c7625b3ba784bbe84ec5cabf772e
Carsten Jonstrup <carsten@lenius.dk>
2016-07-13 01:03:12 +0200

    Update ion_auth.postgre.sql
    When creating new groups you get a error.

--- sql/ion_auth.postgre.sql
+++ sql/ion_auth.postgre.sql
@@ -43,9 +43,9 @@ CREATE TABLE "users_groups" (
 );
 
 
-INSERT INTO groups (id, name, description) VALUES
-    (1,'admin','Administrator'),
-    (2,'members','General User');
+INSERT INTO groups (name, description) VALUES
+    ('admin','Administrator'),
+    ('members','General User');
 
 INSERT INTO users (ip_address, username, password, salt, email, activation_code, forgotten_password_code, created_on, last_login, active, first_name, last_name, company, phone) VALUES
     ('127.0.0.1','administrator','$2a$07$SeBknntpZror9uyftVopmu61qg0ms8Qv1yV6FG.kQOSM.9QhmTo36','','admin@admin.com','',NULL,'1268889823','1268889823','1','Admin','istrator','ADMIN','0');
