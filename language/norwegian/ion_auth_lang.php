8ce47aca5e013e427c6475bc191245fd45f2cea3
Sigve Ringseth <sigve.ringseth@gmail.com>
2016-11-27 17:26:29 +0100

    Update ion_auth_lang.php
    Minor changes.
    Changed the word "email" to "e-post" and updated strings that wasn't translated yet.

--- language/norwegian/ion_auth_lang.php
+++ language/norwegian/ion_auth_lang.php
@@ -22,7 +22,7 @@
 // Account Creation
 $lang['account_creation_successful']			= 'Konto opprettet';
 $lang['account_creation_unsuccessful']			= 'Klarte ikke å opprette konto';
-$lang['account_creation_duplicate_email']		= 'Emailen er allerede i bruk eller ugyldig';
+$lang['account_creation_duplicate_email']		= 'E-posten er allerede i bruk eller ugyldig';
 $lang['account_creation_duplicate_identity']	= 'Brukernavnet er allerede i bruk eller ugyldig';
 $lang['account_creation_missing_default_group'] = 'Standardgruppe er ikke valgt';
 $lang['account_creation_invalid_default_group'] = 'Ugyldig gruppenavn';
@@ -31,7 +31,7 @@ $lang['account_creation_invalid_default_group'] = 'Ugyldig gruppenavn';
 // Password
 $lang['password_change_successful']	  = 'Passordet har blitt endret';
 $lang['password_change_unsuccessful'] = 'Klarte ikke å endre passord';
-$lang['forgot_password_successful']	  = 'Email for tilbakestilling av passord har blitt sendt';
+$lang['forgot_password_successful']	  = 'E-post for tilbakestilling av passord har blitt sendt';
 $lang['forgot_password_unsuccessful'] = 'Klarte ikke å tilbakestille passord';
 
 // Activation
@@ -39,12 +39,12 @@ $lang['activate_successful']		   = 'Kontoen har blitt aktivert';
 $lang['activate_unsuccessful']		   = 'Klarte ikke å aktivere konto';
 $lang['deactivate_successful']		   = 'Kontoen har blitt deaktivert';
 $lang['deactivate_unsuccessful']	   = 'Klarte ikke å deaktivere konto';
-$lang['activation_email_successful']   = 'Email for aktivering av konto har blitt sendt';
-$lang['activation_email_unsuccessful'] = 'Klarte ikke å sende email for aktivering av konto';
+$lang['activation_email_successful']   = 'E-post for aktivering av konto har blitt sendt';
+$lang['activation_email_unsuccessful'] = 'Klarte ikke å sende e-post for aktivering av konto';
 
 // Login / Logout
 $lang['login_successful']			   = 'Logget inn';
-$lang['login_unsuccessful']			   = 'Feil email/brukernavn eller passord';
+$lang['login_unsuccessful']			   = 'Feil e-post/brukernavn eller passord';
 $lang['login_unsuccessful_not_active'] = 'Kontoen er inaktiv';
 $lang['login_timeout']				   = 'Midlertidig sperret. Logg inn senere.';
 $lang['logout_successful']			   = 'Logget ut';
@@ -63,7 +63,7 @@ $lang['group_delete_successful']   = 'Gruppe slettet';
 $lang['group_delete_unsuccessful'] = 'Klarte ikke å slette gruppe';
 $lang['group_delete_notallowed']    = 'Can\'t delete the administrators\' group';
 $lang['group_name_required']	   = 'Gruppenavn må fylles inn';
-$lang['group_name_admin_not_alter'] = 'Admin group name can not be changed';
+$lang['group_name_admin_not_alter'] = 'Admingruppenavnet kan ikke endres';
 
 // Activation Email
 $lang['email_activation_subject']  = 'Aktivering av konto';
