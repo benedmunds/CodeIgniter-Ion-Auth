76ff5df7e4de7380d0eb954955c6df0dba32f234
Filippo Bisconcin <filippo.bisconcin@gmail.com>
2017-01-23 15:00:04 +0100

    reformat + migration

--- language/catalan/ion_auth_lang.php
+++ language/catalan/ion_auth_lang.php
@@ -1,26 +1,28 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-/**
-* Name:  Ion Auth Lang - Catalan
-*
-* Author: Wilfrido García Espinosa
-* 		  contacto@wilfridogarcia.com
-*         @wilfridogarcia
-*
-* Translation: Oriol Navascuez
-*
-* Location: http://github.com/benedmunds/ion_auth/
-*
-* Created:  05.04.2010
-*
-* Description:  Catalan language file for Ion Auth messages and errors
-*
-*/
+<?php
 
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+/**
+ * Name:  Ion Auth Lang - Catalan
+ *
+ * Author: Wilfrido García Espinosa
+ * 		  contacto@wilfridogarcia.com
+ *         @wilfridogarcia
+ *
+ * Translation: Oriol Navascuez
+ *
+ * Location: http://github.com/benedmunds/ion_auth/
+ *
+ * Created:  05.04.2010
+ *
+ * Description:  Catalan language file for Ion Auth messages and errors
+ *
+ */
 // Account Creation
-$lang['account_creation_successful']			= 'Compte creat amb èxit';
-$lang['account_creation_unsuccessful']			= 'No ha estat possible crear al compte';
-$lang['account_creation_duplicate_email']		= 'Email en ús o invàlid';
-$lang['account_creation_duplicate_identity']	= 'Nom d&#39;usuari en ús o invàlid';
+$lang['account_creation_successful'] = 'Compte creat amb èxit';
+$lang['account_creation_unsuccessful'] = 'No ha estat possible crear al compte';
+$lang['account_creation_duplicate_email'] = 'Email en ús o invàlid';
+$lang['account_creation_duplicate_identity'] = 'Nom d&#39;usuari en ús o invàlid';
 
 // TODO Please Translate
 $lang['account_creation_missing_default_group'] = 'Default group is not set';
@@ -28,55 +30,55 @@ $lang['account_creation_invalid_default_group'] = 'Invalid default group name se
 
 
 // Password
-$lang['password_change_successful']				= 'Contrasenya canviada amb èxit';
-$lang['password_change_unsuccessful']			= 'No ha estat possible canviar la contrasenya';
-$lang['forgot_password_successful']				= 'Nova contrasenya enviada per email';
-$lang['forgot_password_unsuccessful']			= 'No ha estat possible crear una nova contrasenya';
+$lang['password_change_successful'] = 'Contrasenya canviada amb èxit';
+$lang['password_change_unsuccessful'] = 'No ha estat possible canviar la contrasenya';
+$lang['forgot_password_successful'] = 'Nova contrasenya enviada per email';
+$lang['forgot_password_unsuccessful'] = 'No ha estat possible crear una nova contrasenya';
 
 // Activation
-$lang['activate_successful']					= 'Compte activat';
-$lang['activate_unsuccessful']					= 'No ha estat possible activar el compte';
-$lang['deactivate_successful']					= 'Compte desactivat';
-$lang['deactivate_unsuccessful']				= 'No ha estat possible desactivar el compte';
-$lang['activation_email_successful']			= 'Email d&#39;activació enviat';
-$lang['activation_email_unsuccessful']			= 'No ha estat possible enviar l&#39;email d&#39;activació';
+$lang['activate_successful'] = 'Compte activat';
+$lang['activate_unsuccessful'] = 'No ha estat possible activar el compte';
+$lang['deactivate_successful'] = 'Compte desactivat';
+$lang['deactivate_unsuccessful'] = 'No ha estat possible desactivar el compte';
+$lang['activation_email_successful'] = 'Email d&#39;activació enviat';
+$lang['activation_email_unsuccessful'] = 'No ha estat possible enviar l&#39;email d&#39;activació';
 
 // Login / Logout
-$lang['login_successful']						= 'Sessió iniciada amb èxit';
-$lang['login_unsuccessful']						= 'No ha estat possible iniciar sessió';
+$lang['login_successful'] = 'Sessió iniciada amb èxit';
+$lang['login_unsuccessful'] = 'No ha estat possible iniciar sessió';
 //TO DO Please translate
-$lang['login_unsuccessful_not_active'] 		 = 'Account is inactive';
-$lang['login_timeout']                       = 'Temporarily Locked Out.  Try again later.';
-$lang['logout_successful']						= 'Sessiò finalitzada amb èxit';
+$lang['login_unsuccessful_not_active'] = 'Account is inactive';
+$lang['login_timeout'] = 'Temporarily Locked Out.  Try again later.';
+$lang['logout_successful'] = 'Sessiò finalitzada amb èxit';
 
 // Account Changes
-$lang['update_successful']						= 'Informació del compte actualitzat amb èxit';
-$lang['update_unsuccessful']					= 'No s&#39;ha pogut actualitzar la informació del compte';
-$lang['delete_successful']						= 'Usuari eliminat';
-$lang['delete_unsuccessful']					= 'No s&#39;ha pogut Eliminar l&#39;usuari';
+$lang['update_successful'] = 'Informació del compte actualitzat amb èxit';
+$lang['update_unsuccessful'] = 'No s&#39;ha pogut actualitzar la informació del compte';
+$lang['delete_successful'] = 'Usuari eliminat';
+$lang['delete_unsuccessful'] = 'No s&#39;ha pogut Eliminar l&#39;usuari';
 
 //TO DO Please translate
 // Groups
-$lang['group_creation_successful']  = 'Group created Successfully';
-$lang['group_already_exists']       = 'Group name already taken';
-$lang['group_update_successful']    = 'Group details updated';
-$lang['group_delete_successful']    = 'Group deleted';
-$lang['group_delete_unsuccessful'] 	= 'Unable to delete group';
-$lang['group_delete_notallowed']    = 'Can\'t delete the administrators\' group';
-$lang['group_name_required'] 		    = 'Group name is a required field';
+$lang['group_creation_successful'] = 'Group created Successfully';
+$lang['group_already_exists'] = 'Group name already taken';
+$lang['group_update_successful'] = 'Group details updated';
+$lang['group_delete_successful'] = 'Group deleted';
+$lang['group_delete_unsuccessful'] = 'Unable to delete group';
+$lang['group_delete_notallowed'] = 'Can\'t delete the administrators\' group';
+$lang['group_name_required'] = 'Group name is a required field';
 $lang['group_name_admin_not_alter'] = 'Admin group name can not be changed';
 
 // Activation Email
-$lang['email_activation_subject']            = 'Activació del compte';
-$lang['email_activate_heading']    = 'Activate account for %s';
+$lang['email_activation_subject'] = 'Activació del compte';
+$lang['email_activate_heading'] = 'Activate account for %s';
 $lang['email_activate_subheading'] = 'Please click this link to %s.';
-$lang['email_activate_link']       = 'Activate Your Account';
+$lang['email_activate_link'] = 'Activate Your Account';
 // Forgot Password Email
-$lang['email_forgotten_password_subject']    = 'Verificació de contrasenya oblidada';
-$lang['email_forgot_password_heading']    = 'Reset Password for %s';
+$lang['email_forgotten_password_subject'] = 'Verificació de contrasenya oblidada';
+$lang['email_forgot_password_heading'] = 'Reset Password for %s';
 $lang['email_forgot_password_subheading'] = 'Please click this link to %s.';
-$lang['email_forgot_password_link']       = 'Reset Your Password';
+$lang['email_forgot_password_link'] = 'Reset Your Password';
 // New Password Email
-$lang['email_new_password_subject']          = 'Nova contrasenya';
-$lang['email_new_password_heading']    = 'New Password for %s';
+$lang['email_new_password_subject'] = 'Nova contrasenya';
+$lang['email_new_password_heading'] = 'New Password for %s';
 $lang['email_new_password_subheading'] = 'Your password has been reset to: %s';
--- libraries/Bcrypt.php
+++ libraries/Bcrypt.php
@@ -1,136 +1,138 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
 
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 
 class Bcrypt {
-  private $rounds;
-  private $salt_prefix;
 
-  /**
-   * Bcrypt constructor.
-   *
-   * @param array $params
-   * @throws Exception
-   */
-  public function __construct($params=array('rounds'=>7, 'salt_prefix'=>'$2y$')) {
+    private $rounds;
+    private $salt_prefix;
 
-    if(CRYPT_BLOWFISH != 1) {
-      throw new Exception("bcrypt not supported in this installation. See http://php.net/crypt");
-    }
-
-    $this->rounds = $params['rounds'];
-    $this->salt_prefix = $params['salt_prefix'];
-  }
-
-  public function hash($input) {
-    $hash = crypt($input, $this->getSalt());
-
-    if(strlen($hash) > 13) {
-      return $hash;
-    }
-
-    return false;
-  }
-
-  /**
-   * @param $input
-   * @param $existingHash
-   * @return bool
+    /**
+     * Bcrypt constructor.
+     *
+     * @param array $params
+     * @throws Exception
      */
-  public function verify($input, $existingHash) {
-    $hash = crypt($input, $existingHash);
-    return $hash === $existingHash;
-  }
+    public function __construct($params = array('rounds' => 7, 'salt_prefix' => '$2y$')) {
 
-  private function getSalt() {
-    $salt = sprintf($this->salt_prefix.'%02d$', $this->rounds);
-
-    $bytes = $this->getRandomBytes(16);
-
-    $salt .= $this->encodeBytes($bytes);
-
-    return $salt;
-  }
-
-  private $randomState;
-
-
-  /**
-   * @param $count
-   * @return string
-     */
-  private function getRandomBytes($count) {
-    $bytes = '';
-
-    if(function_exists('openssl_random_pseudo_bytes') &&
-        (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN')) { // OpenSSL slow on Win
-      $bytes = openssl_random_pseudo_bytes($count);
-    }
-
-    if($bytes === '' && @is_readable('/dev/urandom') &&
-       ($hRand = @fopen('/dev/urandom', 'rb')) !== FALSE) {
-      $bytes = fread($hRand, $count);
-      fclose($hRand);
-    }
-
-    if(strlen($bytes) < $count) {
-      $bytes = '';
-
-      if($this->randomState === null) {
-        $this->randomState = microtime();
-        if(function_exists('getmypid')) {
-          $this->randomState .= getmypid();
+        if (CRYPT_BLOWFISH != 1) {
+            throw new Exception("bcrypt not supported in this installation. See http://php.net/crypt");
         }
-      }
 
-      for($i = 0; $i < $count; $i += 16) {
-        $this->randomState = md5(microtime() . $this->randomState);
-
-        if (PHP_VERSION >= '5') {
-          $bytes .= md5($this->randomState, true);
-        } else {
-          $bytes .= pack('H*', md5($this->randomState));
-        }
-      }
-
-      $bytes = substr($bytes, 0, $count);
+        $this->rounds = $params['rounds'];
+        $this->salt_prefix = $params['salt_prefix'];
     }
 
-    return $bytes;
-  }
+    public function hash($input) {
+        $hash = crypt($input, $this->getSalt());
 
-  /**
-   * @param $input
-   * @return string
+        if (strlen($hash) > 13) {
+            return $hash;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param $input
+     * @param $existingHash
+     * @return bool
      */
-  private function encodeBytes($input) {
-    // The following is code from the PHP Password Hashing Framework
-    $itoa64 = './ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    public function verify($input, $existingHash) {
+        $hash = crypt($input, $existingHash);
+        return $hash === $existingHash;
+    }
 
-    $output = '';
-    $i = 0;
-    do {
-      $c1 = ord($input[$i++]);
-      $output .= $itoa64[$c1 >> 2];
-      $c1 = ($c1 & 0x03) << 4;
-      if ($i >= 16) {
-        $output .= $itoa64[$c1];
-        break;
-      }
+    private function getSalt() {
+        $salt = sprintf($this->salt_prefix . '%02d$', $this->rounds);
 
-      $c2 = ord($input[$i++]);
-      $c1 |= $c2 >> 4;
-      $output .= $itoa64[$c1];
-      $c1 = ($c2 & 0x0f) << 2;
+        $bytes = $this->getRandomBytes(16);
 
-      $c2 = ord($input[$i++]);
-      $c1 |= $c2 >> 6;
-      $output .= $itoa64[$c1];
-      $output .= $itoa64[$c2 & 0x3f];
-    } while (1);
+        $salt .= $this->encodeBytes($bytes);
 
-    return $output;
-  }
+        return $salt;
+    }
+
+    private $randomState;
+
+    /**
+     * @param $count
+     * @return string
+     */
+    private function getRandomBytes($count) {
+        $bytes = '';
+
+        if (function_exists('openssl_random_pseudo_bytes') &&
+                (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN')) { // OpenSSL slow on Win
+            $bytes = openssl_random_pseudo_bytes($count);
+        }
+
+        if ($bytes === '' && @is_readable('/dev/urandom') &&
+                ($hRand = @fopen('/dev/urandom', 'rb')) !== FALSE) {
+            $bytes = fread($hRand, $count);
+            fclose($hRand);
+        }
+
+        if (strlen($bytes) < $count) {
+            $bytes = '';
+
+            if ($this->randomState === null) {
+                $this->randomState = microtime();
+                if (function_exists('getmypid')) {
+                    $this->randomState .= getmypid();
+                }
+            }
+
+            for ($i = 0; $i < $count; $i += 16) {
+                $this->randomState = md5(microtime() . $this->randomState);
+
+                if (PHP_VERSION >= '5') {
+                    $bytes .= md5($this->randomState, true);
+                } else {
+                    $bytes .= pack('H*', md5($this->randomState));
+                }
+            }
+
+            $bytes = substr($bytes, 0, $count);
+        }
+
+        return $bytes;
+    }
+
+    /**
+     * @param $input
+     * @return string
+     */
+    private function encodeBytes($input) {
+        // The following is code from the PHP Password Hashing Framework
+        $itoa64 = './ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+        $output = '';
+        $i = 0;
+        do {
+            $c1 = ord($input[$i++]);
+            $output .= $itoa64[$c1 >> 2];
+            $c1 = ($c1 & 0x03) << 4;
+            if ($i >= 16) {
+                $output .= $itoa64[$c1];
+                break;
+            }
+
+            $c2 = ord($input[$i++]);
+            $c1 |= $c2 >> 4;
+            $output .= $itoa64[$c1];
+            $c1 = ($c2 & 0x0f) << 2;
+
+            $c2 = ord($input[$i++]);
+            $c1 |= $c2 >> 6;
+            $output .= $itoa64[$c1];
+            $output .= $itoa64[$c2 & 0x3f];
+        } while (1);
+
+        return $output;
+    }
+
 }
-
 
 /***** End of BCrypt.php ***********/
