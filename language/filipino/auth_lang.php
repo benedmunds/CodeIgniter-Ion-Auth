fb2e81b842cf0a93c0728bc9640dbeeff1ae10ef
lloricode <emorickfighter@gmail.com>
2017-02-03 09:53:36 +0800

    Filipino Languange

--- language/filipino/auth_lang.php
+++ language/filipino/auth_lang.php
@@ -13,132 +13,132 @@
 *
 * Created:  03.09.2013
 *
-* Description:  Filipino language file for Ion Auth example views
+* Deskripsyon:  Filipino language file for Ion Auth example views
 *
 */
 
 // Errors
-$lang['error_csrf'] = 'This form post did not pass our security checks.';
+$lang['error_csrf'] = 'Ang form na ito post ay hindi pumasa sa aming mga pagsusuri ng seguridad.';
 
 // Login
-$lang['login_heading']         = 'Login';
-$lang['login_subheading']      = 'Please login with your email/username and password below.';
+$lang['login_heading']         = 'Mag-login';
+$lang['login_subheading']      = 'Mangyaring mag-login sa iyong email/username at password sa ibaba.';
 $lang['login_identity_label']  = 'Email/Username:';
 $lang['login_password_label']  = 'Password:';
-$lang['login_remember_label']  = 'Remember Me:';
-$lang['login_submit_btn']      = 'Login';
-$lang['login_forgot_password'] = 'Forgot your password?';
+$lang['login_remember_label']  = 'Tandaan mo ako:';
+$lang['login_submit_btn']      = 'Mag-Login';
+$lang['login_forgot_password'] = 'Nakalimutan ang password?';
 
 // Index
 $lang['index_heading']           = 'Users';
-$lang['index_subheading']        = 'Below is a list of the users.';
-$lang['index_fname_th']          = 'First Name';
-$lang['index_lname_th']          = 'Last Name';
+$lang['index_subheading']        = 'Sa ibaba ay isang listahan ng mga users.';
+$lang['index_fname_th']          = 'Pangalan';
+$lang['index_lname_th']          = 'Huling pangalan';
 $lang['index_email_th']          = 'Email';
-$lang['index_groups_th']         = 'Groups';
-$lang['index_status_th']         = 'Status';
-$lang['index_action_th']         = 'Action';
-$lang['index_active_link']       = 'Active';
-$lang['index_inactive_link']     = 'Inactive';
-$lang['index_create_user_link']  = 'Create a new user';
-$lang['index_create_group_link'] = 'Create a new group';
+$lang['index_groups_th']         = 'Gropo';
+$lang['index_status_th']         = 'katayuan';
+$lang['index_action_th']         = 'aksyon';
+$lang['index_active_link']       = 'aktibo';
+$lang['index_inactive_link']     = 'Hindi aktibo';
+$lang['index_create_user_link']  = 'Lumikha ng isang bagong user';
+$lang['index_create_group_link'] = 'Lumikha ng isang bagong grupo';
 
 // Deactivate User
-$lang['deactivate_heading']                  = 'Deactivate User';
-$lang['deactivate_subheading']               = 'Are you sure you want to deactivate the user \'%s\'';
-$lang['deactivate_confirm_y_label']          = 'Yes:';
-$lang['deactivate_confirm_n_label']          = 'No:';
-$lang['deactivate_submit_btn']               = 'Submit';
-$lang['deactivate_validation_confirm_label'] = 'confirmation';
+$lang['deactivate_heading']                  = 'Huwag paganahin ang User';
+$lang['deactivate_subheading']               = 'Sigurado ka bang gusto mong i-deactivate ang user \'%s\'';
+$lang['deactivate_confirm_y_label']          = 'Oo:';
+$lang['deactivate_confirm_n_label']          = 'Hindi:';
+$lang['deactivate_submit_btn']               = 'Ipasa';
+$lang['deactivate_validation_confirm_label'] = 'pagpapatibay';
 $lang['deactivate_validation_user_id_label'] = 'user ID';
 
 // Create User
-$lang['create_user_heading']                           = 'Create User';
-$lang['create_user_subheading']                        = 'Please enter the user\'s information below.';
-$lang['create_user_fname_label']                       = 'First Name:';
-$lang['create_user_lname_label']                       = 'Last Name:';
-$lang['create_user_company_label']                     = 'Company Name:';
+$lang['create_user_heading']                           = 'Lumikha User';
+$lang['create_user_subheading']                        = 'Mangyaring ipasok ang impormasyon ng user sa ibaba.';
+$lang['create_user_fname_label']                       = 'Pangalan:';
+$lang['create_user_lname_label']                       = 'Huling pangalan:';
+$lang['create_user_company_label']                     = 'Pangalan ng Kumpanya:';
 $lang['create_user_identity_label']                    = 'Identity:';
 $lang['create_user_email_label']                       = 'Email:';
-$lang['create_user_phone_label']                       = 'Phone:';
+$lang['create_user_phone_label']                       = 'Telepono:';
 $lang['create_user_password_label']                    = 'Password:';
-$lang['create_user_password_confirm_label']            = 'Confirm Password:';
-$lang['create_user_submit_btn']                        = 'Create User';
-$lang['create_user_validation_fname_label']            = 'First Name';
-$lang['create_user_validation_lname_label']            = 'Last Name';
+$lang['create_user_password_confirm_label']            = 'kumparahin ang Password:';
+$lang['create_user_submit_btn']                        = 'Lumikha User';
+$lang['create_user_validation_fname_label']            = 'Pangalan';
+$lang['create_user_validation_lname_label']            = 'Huling pangalan';
 $lang['create_user_validation_identity_label']         = 'Identity';
 $lang['create_user_validation_email_label']            = 'Email Address';
-$lang['create_user_validation_phone_label']            = 'Phone';
-$lang['create_user_validation_company_label']          = 'Company Name';
+$lang['create_user_validation_phone_label']            = 'Telepono';
+$lang['create_user_validation_company_label']          = 'Pangalan ng Kumpanya';
 $lang['create_user_validation_password_label']         = 'Password';
 $lang['create_user_validation_password_confirm_label'] = 'Password Confirmation';
 
 // Edit User
 $lang['edit_user_heading']                           = 'Edit User';
 $lang['edit_user_subheading']                        = 'Please enter the user\'s information below.';
-$lang['edit_user_fname_label']                       = 'First Name:';
-$lang['edit_user_lname_label']                       = 'Last Name:';
-$lang['edit_user_company_label']                     = 'Company Name:';
+$lang['edit_user_fname_label']                       = 'Pangalan:';
+$lang['edit_user_lname_label']                       = 'Huling pangalan:';
+$lang['edit_user_company_label']                     = 'Pangalan ng Kumpanya:';
 $lang['edit_user_email_label']                       = 'Email:';
-$lang['edit_user_phone_label']                       = 'Phone:';
-$lang['edit_user_password_label']                    = 'Password: (if changing password)';
-$lang['edit_user_password_confirm_label']            = 'Confirm Password: (if changing password)';
-$lang['edit_user_groups_heading']                    = 'Member of groups';
-$lang['edit_user_submit_btn']                        = 'Save User';
-$lang['edit_user_validation_fname_label']            = 'First Name';
-$lang['edit_user_validation_lname_label']            = 'Last Name';
+$lang['edit_user_phone_label']                       = 'Telepono:';
+$lang['edit_user_password_label']                    = 'Password: (kung ang pagbabago password)';
+$lang['edit_user_password_confirm_label']            = 'Kumpirmahin ang Password: (kung pagbabago ng password)';
+$lang['edit_user_groups_heading']                    = 'Miyembro ng grupo';
+$lang['edit_user_submit_btn']                        = 'I-save ang User';
+$lang['edit_user_validation_fname_label']            = 'Pangalan';
+$lang['edit_user_validation_lname_label']            = 'Huling pangalan';
 $lang['edit_user_validation_email_label']            = 'Email Address';
-$lang['edit_user_validation_phone_label']            = 'Phone';
-$lang['edit_user_validation_company_label']          = 'Company Name';
-$lang['edit_user_validation_groups_label']           = 'Groups';
+$lang['edit_user_validation_phone_label']            = 'Telepono';
+$lang['edit_user_validation_company_label']          = 'Pangalan ng Kumpanya';
+$lang['edit_user_validation_groups_label']           = 'Grupo';
 $lang['edit_user_validation_password_label']         = 'Password';
 $lang['edit_user_validation_password_confirm_label'] = 'Password Confirmation';
 
-// Create Group
-$lang['create_group_title']                  = 'Create Group';
-$lang['create_group_heading']                = 'Create Group';
-$lang['create_group_subheading']             = 'Please enter the group information below.';
-$lang['create_group_name_label']             = 'Group Name:';
-$lang['create_group_desc_label']             = 'Description:';
-$lang['create_group_submit_btn']             = 'Create Group';
-$lang['create_group_validation_name_label']  = 'Group Name';
-$lang['create_group_validation_desc_label']  = 'Description';
+// Gumawa ng Grupo
+$lang['create_group_title']                  = 'Gumawa ng Grupo';
+$lang['create_group_heading']                = 'Gumawa ng Grupo';
+$lang['create_group_subheading']             = 'Mangyaring ipasok ang impormasyon ng grupo sa ibaba.';
+$lang['create_group_name_label']             = 'Pangalan ng Grupo:';
+$lang['create_group_desc_label']             = 'Deskripsyon:';
+$lang['create_group_submit_btn']             = 'Gumawa ng Grupo';
+$lang['create_group_validation_name_label']  = 'Pangalan ng Grupo';
+$lang['create_group_validation_desc_label']  = 'Deskripsyon';
 
-// Edit Group
-$lang['edit_group_title']                  = 'Edit Group';
-$lang['edit_group_saved']                  = 'Group Saved';
-$lang['edit_group_heading']                = 'Edit Group';
-$lang['edit_group_subheading']             = 'Please enter the group information below.';
-$lang['edit_group_name_label']             = 'Group Name:';
-$lang['edit_group_desc_label']             = 'Description:';
-$lang['edit_group_submit_btn']             = 'Save Group';
-$lang['edit_group_validation_name_label']  = 'Group Name';
-$lang['edit_group_validation_desc_label']  = 'Description';
+// Baguhin ang Grupo
+$lang['edit_group_title']                  = 'Baguhin ang Grupo';
+$lang['edit_group_saved']                  = 'Group Nai-save na';
+$lang['edit_group_heading']                = 'Baguhin ang Grupo';
+$lang['edit_group_subheading']             = 'Mangyaring ipasok ang impormasyon ng grupo sa ibaba.';
+$lang['edit_group_name_label']             = 'Pangalan ng Grupo:';
+$lang['edit_group_desc_label']             = 'Deskripsyon:';
+$lang['edit_group_submit_btn']             = 'I-save ang Group';
+$lang['edit_group_validation_name_label']  = 'Pangalan ng Grupo';
+$lang['edit_group_validation_desc_label']  = 'Deskripsyon';
 
-// Change Password
-$lang['change_password_heading']                               = 'Change Password';
-$lang['change_password_old_password_label']                    = 'Old Password:';
-$lang['change_password_new_password_label']                    = 'New Password (at least %s characters long):';
-$lang['change_password_new_password_confirm_label']            = 'Confirm New Password:';
-$lang['change_password_submit_btn']                            = 'Change';
-$lang['change_password_validation_old_password_label']         = 'Old Password';
-$lang['change_password_validation_new_password_label']         = 'New Password';
-$lang['change_password_validation_new_password_confirm_label'] = 'Confirm New Password';
+// Palitan ang Password
+$lang['change_password_heading']                               = 'Palitan ang Password';
+$lang['change_password_old_password_label']                    = 'Lumang Password:';
+$lang['change_password_new_password_label']                    = 'Bagong Password (at least %s characters long):';
+$lang['change_password_new_password_confirm_label']            = 'kumparahin ang Bagong Password:';
+$lang['change_password_submit_btn']                            = 'Palitan';
+$lang['change_password_validation_old_password_label']         = 'Lumang Password';
+$lang['change_password_validation_new_password_label']         = 'Bagong Password';
+$lang['change_password_validation_new_password_confirm_label'] = 'kumparahin ang Bagong Password';
 
 // Forgot Password
 $lang['forgot_password_heading']                 = 'Forgot Password';
-$lang['forgot_password_subheading']              = 'Please enter your %s so we can send you an email to reset your password.';
+$lang['forgot_password_subheading']              = 'Pakipasok ang iyong %s upang maaari naming ipasa sa iyong email ang mensahe upang i-reset ang iyong password.';
 $lang['forgot_password_email_label']             = '%s:';
 $lang['forgot_password_submit_btn']              = 'Submit';
 $lang['forgot_password_validation_email_label']  = 'Email Address';
 $lang['forgot_password_identity_label'] = 'Identity';
 $lang['forgot_password_email_identity_label']    = 'Email';
-$lang['forgot_password_email_not_found']         = 'No record of that email address.';
+$lang['forgot_password_email_not_found']         = 'Walang record ng email address.';
 
 // Reset Password
-$lang['reset_password_heading']                               = 'Change Password';
-$lang['reset_password_new_password_label']                    = 'New Password (at least %s characters long):';
-$lang['reset_password_new_password_confirm_label']            = 'Confirm New Password:';
-$lang['reset_password_submit_btn']                            = 'Change';
-$lang['reset_password_validation_new_password_label']         = 'New Password';
-$lang['reset_password_validation_new_password_confirm_label'] = 'Confirm New Password';
+$lang['reset_password_heading']                               = 'Palitan ang Password';
+$lang['reset_password_new_password_label']                    = 'Bagong Password (at least %s characters long):';
+$lang['reset_password_new_password_confirm_label']            = 'kumparahin ang Bagong Password:';
+$lang['reset_password_submit_btn']                            = 'Palitan';
+$lang['reset_password_validation_new_password_label']         = 'Bagong Password';
+$lang['reset_password_validation_new_password_confirm_label'] = 'kumparahin ang Bagong Password';
--- language/filipino/ion_auth_lang.php
+++ language/filipino/ion_auth_lang.php
@@ -15,65 +15,65 @@
 */
 
 // Account Creation
-$lang['account_creation_successful']            = 'Account Successfully Created';
-$lang['account_creation_unsuccessful']          = 'Unable to Create Account';
-$lang['account_creation_duplicate_email']       = 'Email Already Used or Invalid';
-$lang['account_creation_duplicate_identity']    = 'Identity Already Used or Invalid';
-$lang['account_creation_missing_default_group'] = 'Default group is not set';
-$lang['account_creation_invalid_default_group'] = 'Invalid default group name set';
+$lang['account_creation_successful']            = 'Matagumpay na nagawa ang account';
+$lang['account_creation_unsuccessful']          = 'Hindi magawang i-Lumikha ng Account';
+$lang['account_creation_duplicate_email']       = 'Email ay Nagamit na o Hindi wastong';
+$lang['account_creation_duplicate_identity']    = 'Pagkakakilanlan ay Nagamit na o Hindi wastong';
+$lang['account_creation_missing_default_group'] = 'Default na grupo ay hindi nakatakda';
+$lang['account_creation_invalid_default_group'] = 'Hindi wasto ang default na ngalan ng grupo';
 
 
 // Password
-$lang['password_change_successful']          = 'Password Successfully Changed';
-$lang['password_change_unsuccessful']        = 'Unable to Change Password';
-$lang['forgot_password_successful']          = 'Password Reset Email Sent';
-$lang['forgot_password_unsuccessful']        = 'Unable to Reset Password';
+$lang['password_change_successful']          = 'Password Matagumpay Binago';
+$lang['password_change_unsuccessful']        = 'Hindi ma-Baguhin ang Password';
+$lang['forgot_password_successful']          = 'Password Reset ay na-send na sa Email';
+$lang['forgot_password_unsuccessful']        = 'Hindi ma-reset ang Password';
 
 // Activation
-$lang['activate_successful']                 = 'Account Activated';
-$lang['activate_unsuccessful']               = 'Unable to Activate Account';
+$lang['activate_successful']                 = 'Account napagana na';
+$lang['activate_unsuccessful']               = 'Hindi ma-activate Account';
 $lang['deactivate_successful']               = 'Account De-Activated';
-$lang['deactivate_unsuccessful']             = 'Unable to De-Activate Account';
-$lang['activation_email_successful']         = 'Activation Email Sent. Please check your inbox or spam';
-$lang['activation_email_unsuccessful']       = 'Unable to Send Activation Email';
-$lang['deactivate_current_user_unsuccessful']= 'You cannot De-Activate your self.';
+$lang['deactivate_unsuccessful']             = 'Hindi ma-De-Activate Account';
+$lang['activation_email_successful']         = 'Activation Email Sent. Mangyaring suriin ang iyong inbox o spam';
+$lang['activation_email_unsuccessful']       = 'Hindi magawang Magpadala Activation Email';
+$lang['deactivate_current_user_unsuccessful']= 'Hindi mo Maaaring De-activate ang iyong sarili.';
 
 // Login / Logout
-$lang['login_successful']                    = 'Logged In Successfully';
-$lang['login_unsuccessful']                  = 'Incorrect Login';
-$lang['login_unsuccessful_not_active']       = 'Account is inactive';
-$lang['login_timeout']                       = 'Temporarily Locked Out.  Try again later.';
-$lang['logout_successful']                   = 'Logged Out Successfully';
+$lang['login_successful']                    = 'Tagumpay na Naka-Login';
+$lang['login_unsuccessful']                  = 'Maling Login';
+$lang['login_unsuccessful_not_active']       = 'Account ay hindi aktibo';
+$lang['login_timeout']                       = 'Pansamantalang Naka-lock Out. Subukan ulit mamaya.';
+$lang['logout_successful']                   = 'Matagumpay na Naka-log Out';
 
 // Account Changes
-$lang['update_successful']                   = 'Account Information Successfully Updated';
-$lang['update_unsuccessful']                 = 'Unable to Update Account Information';
-$lang['delete_successful']                   = 'User Deleted';
-$lang['delete_unsuccessful']                 = 'Unable to Delete User';
+$lang['update_successful']                   = 'Impormasyon ng Account Matagumpay Na-Bago';
+$lang['update_unsuccessful']                 = 'Hindi ma-update ang Impormasyon ng Account';
+$lang['delete_successful']                   = 'User Natanggal na';
+$lang['delete_unsuccessful']                 = 'Hindi magawang alisin User';
 
 // Groups
-$lang['group_creation_successful']           = 'Group created Successfully';
-$lang['group_already_exists']                = 'Group name already taken';
-$lang['group_update_successful']             = 'Group details updated';
-$lang['group_delete_successful']             = 'Group deleted';
-$lang['group_delete_unsuccessful']           = 'Unable to delete group';
-$lang['group_delete_notallowed']             = 'Can\'t delete the administrators\' group';
-$lang['group_name_required']                 = 'Group name is a required field';
-$lang['group_name_admin_not_alter']          = 'Admin group name can not be changed';
+$lang['group_creation_successful']           = 'Matagumpay na Nalikha ang Grupo';
+$lang['group_already_exists']                = 'Ang pangalan ng grupo nagamit na';
+$lang['group_update_successful']             = 'Detalye sa Grupo Na-Bago na';
+$lang['group_delete_successful']             = 'Ang Grupo Na-Tanggal na';
+$lang['group_delete_unsuccessful']           = 'Hindi matanggal ang grupo';
+$lang['group_delete_notallowed']             = 'Hindi Maaaring tanggalin ang Grupo Administrator';
+$lang['group_name_required']                 = 'Ang Ngalan sa Grupo ay Kailangan';
+$lang['group_name_admin_not_alter']          = 'Hndi Maaaring Palitan ang Ngalan sa Grupo';
 
 // Activation Email
 $lang['email_activation_subject']            = 'Account Activation';
-$lang['email_activate_heading']              = 'Activate account for %s';
-$lang['email_activate_subheading']           = 'Please click this link to %s.';
-$lang['email_activate_link']                 = 'Activate Your Account';
+$lang['email_activate_heading']              = 'I-activate account para sa %s';
+$lang['email_activate_subheading']           = 'Mangyaring i-click ang link na ito  %s.';
+$lang['email_activate_link']                 = 'I-activate ang Iyong Account';
 
 // Forgot Password Email
-$lang['email_forgotten_password_subject']    = 'Forgotten Password Verification';
-$lang['email_forgot_password_heading']       = 'Reset Password for %s';
-$lang['email_forgot_password_subheading']    = 'Please click this link to %s.';
-$lang['email_forgot_password_link']          = 'Reset Your Password';
+$lang['email_forgotten_password_subject']    = 'Nakalimutan mo ba ang Verification Password';
+$lang['email_forgot_password_heading']       = 'I-reset ang Password para sa %s';
+$lang['email_forgot_password_subheading']    = 'Mangyaring i-click ang link na ito  %s.';
+$lang['email_forgot_password_link']          = 'I-reset ang Iyong Password';
 
 // New Password Email
-$lang['email_new_password_subject']          = 'New Password';
-$lang['email_new_password_heading']          = 'New Password for %s';
-$lang['email_new_password_subheading']       = 'Your password has been reset to: %s';
+$lang['email_new_password_subject']          = 'Bagong Password';
+$lang['email_new_password_heading']          = 'Bagong Password para sa %s';
+$lang['email_new_password_subheading']       = 'Ang iyong password ay i-reset sa: %s';
