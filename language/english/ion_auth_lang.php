8fb27e5551d4746475fabe9b8c5c8bc20a274d92
Adrian Voicu <avenir.ro@gmail.com>
2017-02-07 09:37:34 +0200

    More specific message for unsuccessful send
    This should be a more specific error description. @benedmunds maybe you can find a better message.

--- language/english/ion_auth_lang.php
+++ language/english/ion_auth_lang.php
@@ -27,7 +27,7 @@ $lang['account_creation_invalid_default_group'] = 'Invalid default group name se
 $lang['password_change_successful']          = 'Password Successfully Changed';
 $lang['password_change_unsuccessful']        = 'Unable to Change Password';
 $lang['forgot_password_successful']          = 'Password Reset Email Sent';
-$lang['forgot_password_unsuccessful']        = 'Unable to Reset Password';
+$lang['forgot_password_unsuccessful']        = 'Unable to email the Reset Password link';
 
 // Activation
 $lang['activate_successful']                 = 'Account Activated';
