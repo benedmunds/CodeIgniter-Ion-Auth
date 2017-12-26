04c194ac06118470cb295e29fc42b0b2f5a72013
Federico Villa <federico.villa.1966@gmail.com>
2017-12-26 09:18:23 +0100

    Update ion_auth_lang.php

--- language/italian/ion_auth_lang.php
+++ language/italian/ion_auth_lang.php
@@ -19,8 +19,6 @@ $lang['account_creation_successful'] 	  	 = 'Account creato con successo.';
 $lang['account_creation_unsuccessful'] 	 	 = 'Impossibile creare l\'account.';
 $lang['account_creation_duplicate_email'] 	 = 'Email gi&agrave; in uso o non valida.';
 $lang['account_creation_duplicate_identity'] 	 = 'Nome utente gi&agrave; in uso o non valido.';
-
-// TODO Please Translate
 $lang['account_creation_missing_default_group'] = 'Gruppo predefinito non impostato';
 $lang['account_creation_invalid_default_group'] = 'Nome del gruppo predefinito non valido';
 
