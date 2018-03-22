100b6abee14ed3b2cb96e344fa1100d2dcda8d96
Indigo744 <contact-github@gperrin.fr>
2018-03-22 16:46:01 +0100

    Set cookie expire as computed value

--- models/Ion_auth_model.php
+++ models/Ion_auth_model.php
@@ -1829,7 +1829,7 @@ class Ion_auth_model extends CI_Model
 		// if the user_expire is set to zero we'll set the expiration two years from now.
 		if($this->config->item('user_expire', 'ion_auth') === 0)
 		{
-			$expire = (60*60*24*365*2);
+			$expire = 63072000; // 2 years = 60*60*24*365*2 = 63072000 seconds
 		}
 		// otherwise use what is set
 		else
