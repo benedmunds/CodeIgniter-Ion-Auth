670a22cd6f5d8424a1f799897e71325d4ea56da9
Indigo744 <contact-github@gperrin.fr>
2018-03-22 17:41:13 +0100

    Create SQL upgrading scripts

---
+++ sql/migrating_from_ionauth2/migrate.sql
@@ -0,0 +1,30 @@
+
+#
+# Remember_me feature upgrade
+#
+ALTER TABLE `users`
+  ADD     COLUMN `remember_selector`  varchar(255) AFTER `forgotten_password_time`,
+  MODIFY  COLUMN `remember_code`      varchar(255);
+
+#
+# Forgotten password feature upgrade
+#
+ALTER TABLE `users`
+  ADD     COLUMN `forgotten_password_selector` varchar(255) AFTER `activation_code`,
+  MODIFY  COLUMN `forgotten_password_code`     varchar(255);
+
+#
+# Add optimization
+#
+ALTER TABLE `users`
+  ADD CONSTRAINT uc_email
+    UNIQUE (email);
+
+ALTER TABLE `users`
+  ADD CONSTRAINT uc_remember_selector
+    UNIQUE (remember_selector);
+
+ALTER TABLE `users`
+  ADD CONSTRAINT uc_forgotten_password_selector
+    UNIQUE (forgotten_password_selector);
+
