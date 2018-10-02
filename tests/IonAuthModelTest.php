798af6444564b55d3d55e36595db1c0aec48edca
Benoit VRIGNAUD <benoit.vrignaud@zaclys.net>
2018-10-02 11:18:28 +0200

    Add model units tests

---
+++ tests/IonAuthModelTest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * IonAuthModel tests
+ *
+ * @package CodeIgniter-Ion-Auth
+ * @author  Benoit VRIGNAUD <benoit.vrignaud@zaclys.net>
+ * @license https://opensource.org/licenses/MIT	MIT License
+ */
+
+use App\Models\IonAuthModel;
+
+/**
+ * IonAuthModel tests
+ *
+ * @package CodeIgniter-Ion-Auth
+ */
+class IonAuthModelTest extends \CodeIgniter\Test\CIDatabaseTestCase
+{
+	/**
+	 * Should the db be refreshed before
+	 * each test?
+	 *
+	 * @var boolean
+	 */
+	protected $refresh = false;
+
+	/**
+	 * Test emailCheck
+	 *
+	 * @return void
+	 */
+	public function testEmailCheck()
+	{
+		$model = new IonAuthModel();
+		$this->assertTrue($model->emailCheck('admin@admin.com'));
+		$this->assertFalse($model->emailCheck(''));
+		$this->assertFalse($model->emailCheck('email@undefined.unknown'));
+	}
+}
