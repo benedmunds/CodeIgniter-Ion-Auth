2c9eede16a2f5e8e055748e8b3d76141a3a7cec0
Camilo Caquimbo Tabares <33238233+camilocaquimbo@users.noreply.github.com>
2019-03-14 16:20:07 -0500

    Update ion_auth_lang.php
    Traduction

--- language/spanish/ion_auth_lang.php
+++ language/spanish/ion_auth_lang.php
@@ -35,7 +35,7 @@ $lang['deactivate_successful']                  = 'Cuenta desactivada con éxito
 $lang['deactivate_unsuccessful']                = 'No se ha podido desactivar la cuenta';
 $lang['activation_email_successful']            = 'Email de activación enviado';
 $lang['activation_email_unsuccessful']          = 'No se ha podido enviar el email de activación';
-$lang['deactivate_current_user_unsuccessful']= 'You cannot De-Activate your self.';
+$lang['deactivate_current_user_unsuccessful']= 'No se puede desactivar usted mismo';
 
 // Login / Logout
 $lang['login_successful']                       = 'Sesión iniciada con éxito';
