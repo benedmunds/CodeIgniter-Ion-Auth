951c116e4072d68ac8c829c192176679bb42a2f7
Donatas Glodenis <dgvirtual@akl.lt>
2024-01-19 22:49:57 +0200

    Update translation to Lithuanian

--- Language/lt/Auth.php
+++ Language/lt/Auth.php
@@ -7,12 +7,17 @@
 *
 * Location: http://github.com/benedmunds/ion_auth/
 *
-* Created:  10.05.2016
-* Updated: 14.04.2020
+* Created:  2016-05-10
+* Updated:  2024-01-19
 *
 * Description:  Lithuanian language file for Ion Auth example views
 *
 */
+
+
+
+
+
 
 return [
 	// Errors
@@ -21,9 +26,9 @@ return [
 	// Login
 	'login_heading'         => 'Prisijungimas',
 	'login_subheading'      => 'Prašome prisijungti įrašant el. pašto adresą arba prisijungimo vardą ir slaptažodį.',
-	'login_identity_label'  => 'El. pašto adresas / vartotojo vardas:',
-	'login_password_label'  => 'Slaptažodis:',
-	'login_remember_label'  => 'Atsiminti mane:',
+	'login_identity_label'  => 'El. pašto adresas / vartotojo vardas',
+	'login_password_label'  => 'Slaptažodis',
+	'login_remember_label'  => 'atsiminti mane',
 	'login_submit_btn'      => 'Prisijungti',
 	'login_forgot_password' => 'Pamiršote slaptažodį?',
 
@@ -44,7 +49,7 @@ return [
 
 	// Deactivate User
 	'deactivate_heading'                  => 'Išjungti vartotoją',
-	'deactivate_subheading'               => 'Ar tikrai norite išjungti vartotoją \'%s\'',
+	'deactivate_subheading'               => 'Ar tikrai norite išjungti vartotoją „%s“?',
 	'deactivate_confirm_y_label'          => 'Taip:',
 	'deactivate_confirm_n_label'          => 'Ne:',
 	'deactivate_submit_btn'               => 'Pateikti',
@@ -54,19 +59,19 @@ return [
 	// Create User
 	'create_user_heading'                           => 'Sukurti vartotoją',
 	'create_user_subheading'                        => 'Prašome įrašyti vartotojo informaciją.',
-	'create_user_fname_label'                       => 'Vardas:',
-	'create_user_lname_label'                       => 'Pavardė:',
-	'create_user_company_label'                     => 'Įmonė:',
-	'create_user_identity_label'                    => 'Tapatybė:',
-	'create_user_email_label'                       => 'El. p. adresas:',
-	'create_user_phone_label'                       => 'Telefonas:',
-	'create_user_password_label'                    => 'Slaptažodis:',
-	'create_user_password_confirm_label'            => 'Patvirtinti slaptažodį:',
+	'create_user_fname_label'                       => 'Vardas',
+	'create_user_lname_label'                       => 'Pavardė',
+	'create_user_company_label'                     => 'Įmonė',
+	'create_user_identity_label'                    => 'Tapatybė',
+	'create_user_email_label'                       => 'El. p. adresas',
+	'create_user_phone_label'                       => 'Telefonas',
+	'create_user_password_label'                    => 'Slaptažodis',
+	'create_user_password_confirm_label'            => 'Patvirtinti slaptažodį',
 	'create_user_submit_btn'                        => 'Sukurti vartotoją',
 	'create_user_validation_fname_label'            => 'Vardas',
 	'create_user_validation_lname_label'            => 'Pavardė',
 	'create_user_validation_identity_label'         => 'Tapatybė',
-	'create_user_validation_email_label'            => 'El. p. adresas:',
+	'create_user_validation_email_label'            => 'El. p. adresas',
 	'create_user_validation_phone_label'            => 'Telefonas',
 	'create_user_validation_company_label'          => 'Įmonės pavadinimas',
 	'create_user_validation_password_label'         => 'Slaptažodis',
@@ -75,13 +80,13 @@ return [
 	// Edit User
 	'edit_user_heading'                           => 'Keisti vartotojo duomenis',
 	'edit_user_subheading'                        => 'Prašome įrašyti vartotojo informaciją.',
-	'edit_user_fname_label'                       => 'Vardas:',
-	'edit_user_lname_label'                       => 'Pavardė:',
-	'edit_user_company_label'                     => 'Įmonės pavadinimas:',
-	'edit_user_email_label'                       => 'El. p. adresas:',
-	'edit_user_phone_label'                       => 'Telefono Nr.:',
-	'edit_user_password_label'                    => 'Slaptažodis: (jei keičiamas slaptažodis)',
-	'edit_user_password_confirm_label'            => 'Patvirtinti slaptažodį: (jei keičiamas slaptažodis)',
+	'edit_user_fname_label'                       => 'Vardas',
+	'edit_user_lname_label'                       => 'Pavardė',
+	'edit_user_company_label'                     => 'Įmonės pavadinimas',
+	'edit_user_email_label'                       => 'El. p. adresas',
+	'edit_user_phone_label'                       => 'Telefono Nr.',
+	'edit_user_password_label'                    => 'Slaptažodis (jei keičiamas slaptažodis)',
+	'edit_user_password_confirm_label'            => 'Patvirtinti slaptažodį (jei keičiamas slaptažodis)',
 	'edit_user_groups_heading'                    => 'Grupių narys',
 	'edit_user_submit_btn'                        => 'Įrašyti vartotoją',
 	'edit_user_validation_fname_label'            => 'Vardas',
@@ -97,48 +102,48 @@ return [
 	'create_group_title'                  => 'Sukurti grupę',
 	'create_group_heading'                => 'Sukurti grupę',
 	'create_group_subheading'             => 'Prašome įrašyti grupės informaciją.',
-	'create_group_name_label'             => 'Grupės pavadinimas:',
-	'create_group_desc_label'             => 'Aprašymas:',
+	'create_group_name_label'             => 'Grupės pavadinimas',
+	'create_group_desc_label'             => 'Aprašymas',
 	'create_group_submit_btn'             => 'Sukurti grupę',
 	'create_group_validation_name_label'  => 'Grupės pavadinimas',
 	'create_group_validation_desc_label'  => 'Aprašymas',
 
 	// Edit Group
-	'edit_group_title'                  => 'Keisti grupę',
-	'edit_group_saved'                  => 'Grupė įrašyta',
-	'edit_group_heading'                => 'Keisti grupę',
-	'edit_group_subheading'             => 'Prašome įrašyti grupės informaciją.',
-	'edit_group_name_label'             => 'Grupės pavadinimas:',
-	'edit_group_desc_label'             => 'Aprašymas:',
-	'edit_group_submit_btn'             => 'Įrašyti grupę',
-	'edit_group_validation_name_label'  => 'Grupės pavadinimas',
-	'edit_group_validation_desc_label'  => 'Aprašymas',
+	'edit_group_title'                 => 'Keisti grupę',
+	'edit_group_saved'                 => 'Grupė įrašyta',
+	'edit_group_heading'               => 'Keisti grupę',
+	'edit_group_subheading'            => 'Prašome įrašyti grupės informaciją.',
+	'edit_group_name_label'            => 'Grupės pavadinimas',
+	'edit_group_desc_label'            => 'Aprašymas',
+	'edit_group_submit_btn'            => 'Įrašyti grupę',
+	'edit_group_validation_name_label' => 'Grupės pavadinimas',
+	'edit_group_validation_desc_label' => 'Aprašymas',
 
 	// Change Password
-	'change_password_heading'                               => 'Pakeisti slaptažodį',
-	'change_password_old_password_label'                    => 'Senas slaptažodis:',
-	'change_password_new_password_label'                    => 'Naujas slaptažodis (mažiausiai %s simbolių):',
-	'change_password_new_password_confirm_label'            => 'Patvirtinti naują slaptažodį:',
+	'change_password_heading'                               => 'Keisti slaptažodį',
+	'change_password_old_password_label'                    => 'Dabartinis slaptažodis',
+	'change_password_new_password_label'                    => 'Naujas slaptažodis (bent %s simbolių)',
+	'change_password_new_password_confirm_label'            => 'Patvirtinti naują slaptažodį',
 	'change_password_submit_btn'                            => 'Pakeisti',
-	'change_password_validation_old_password_label'         => 'Senas slaptažodis',
+	'change_password_validation_old_password_label'         => 'Dabartinis slaptažodis',
 	'change_password_validation_new_password_label'         => 'Naujas slaptažodis',
 	'change_password_validation_new_password_confirm_label' => 'Patvirtinti naują slaptažodį',
 
 	// Forgot Password
-	'forgot_password_heading'                 => 'Pamiršus slaptažodį',
-	'forgot_password_subheading'              => 'Prašome įrašyti savo %s kad galėtume išsiųsti Jums el. laišką slaptažodžio atkūrimui.',
-	'forgot_password_email_label'             => '%s:',
-	'forgot_password_submit_btn'              => 'Pateikti',
-	'forgot_password_validation_email_label'  => 'El. p. adresas',
-	'forgot_password_identity_label'          => 'Vartotojo vardas',
-	'forgot_password_email_identity_label'    => 'El. p. adresas',
-	'forgot_password_email_not_found'         => 'Duomenų bazėje tokio el. pašto adreso nėra.',
-	'forgot_password_identity_not_found'      => 'Nėra įrašo apie su šiuo vartotojo vardu susietą adresą.',
+	'forgot_password_heading'                => 'Pamiršus slaptažodį',
+	'forgot_password_subheading'             => 'Prašome įrašyti savo %s, kad galėtume išsiųsti Jums el. laišką slaptažodžio atkūrimui.',
+	'forgot_password_email_label'            => '%s:',
+	'forgot_password_submit_btn'             => 'Pateikti',
+	'forgot_password_validation_email_label' => 'El. p. adresas',
+	'forgot_password_identity_label'         => 'Vartotojo vardas',
+	'forgot_password_email_identity_label'   => 'El. p. adresas',
+	'forgot_password_email_not_found'        => 'Duomenų bazėje tokio el. pašto adreso nėra.',
+	'forgot_password_identity_not_found'     => 'Nėra įrašo apie su šiuo vartotojo vardu susietą adresą.',
 
 	// Reset password
 	'reset_password_heading'                               => 'Pakeisti slaptažodį',
-	'reset_password_new_password_label'                    => 'Naujas slaptažodis (mažiausiai %s simboliai/-ių):',
-	'reset_password_new_password_confirm_label'            => 'Patvirtinti naują slaptažodį:',
+	'reset_password_new_password_label'                    => 'Naujas slaptažodis (bent %s simboliai/-ų)',
+	'reset_password_new_password_confirm_label'            => 'Patvirtinti naują slaptažodį',
 	'reset_password_submit_btn'                            => 'Keisti',
 	'reset_password_validation_new_password_label'         => 'Naujas slaptažodis',
 	'reset_password_validation_new_password_confirm_label' => 'Patvirtinti naują slaptažodį',
